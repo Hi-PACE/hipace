d0124758af3693bad23653967e60413059525573
MaxThevenet <mthevenet@lbl.gov>
2020-05-21 11:35:53 -0700

    Hipace derives from AmrCore

---
+++ src/Hipace.cpp
@@ -0,0 +1,14 @@
+#include "Hipace.H"
+
+void
+Hipace::InitData ()
+{
+
+}
+
+void
+Hipace::MakeNewLevelFromScratch (
+    int lev, amrex::Real time, const amrex::BoxArray& ba, const amrex::DistributionMapping& dm)
+{
+    
+}
