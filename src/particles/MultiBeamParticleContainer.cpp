70f337740b00f62b037c50887b481df39f491717
Andrew Myers <atmyers2@gmail.com>
2020-05-21 11:42:00 -0700

    add the multi beam particle container

---
+++ src/particles/MultiBeamParticleContainer.cpp
@@ -0,0 +1,11 @@
+#include "MultiBeamParticleContainer.H"
+
+MultiParticleContainer::MultiParticleContainer (AmrCore* amr_core)
+{
+    constexpr int nbeams = 1;
+    allcontainers.resize(nbeams);
+    for (int i = 0; i < nspecies; ++i)
+    {
+        allcontainers[i].reset(new BeamParticleContainer(amr_core);
+    }
+};
