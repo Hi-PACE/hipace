1ea40e793f9495835f4b20b25f4dee09f5763e49
MaxThevenet <mthevenet@lbl.gov>
2020-05-21 13:01:02 -0700

    fixes so it compiles

--- src/fields/Fields.cpp
+++ src/fields/Fields.cpp
@@ -3,5 +3,5 @@
 void
 Fields::AllocData (int lev, const amrex::BoxArray& ba, const amrex::DistributionMapping& dm)
 {
-    m_F[lev] = amrex::MultiFab(ba, dm, FieldComp::nfields, m_nguards);
+    m_F[lev] = amrex::MultiFab(ba, dm, FieldComps::nfields, m_nguards);
 }
