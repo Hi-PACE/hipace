4798bce59dbe0659c35546f9ad19f930f2061b12
MaxThevenet <mthevenet@lbl.gov>
2020-05-21 11:11:45 -0700

    add main function and hipace clss

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
-int main()
+
+#include "Hipace.H"
+
+#include <AMReX.H>
+
+using namespace amrex;
+
+int main (int argc, char* argv[])
 {
+    amrex::Initialize(argc,argv);
+    {
+        Hipace hipace;
+        hipace.InitData();
+    }
+    amrex::Finalize();
 }
