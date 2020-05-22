03bc6db47c1774d9edde46bd8e6e787e26aa9877
MaxThevenet <mthevenet@lbl.gov>
2020-05-22 15:31:44 -0700

    add plotting script, to show results after running simulation

---
+++ examples/show.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+# Plots Ex field and particles at the end of the run,
+# and save image to img.png
+# usage: ./show.py
+
+import yt ; yt.funcs.mylog.setLevel(50)
+import matplotlib.pyplot as plt
+from yt.frontends.boxlib.data_structures import AMReXDataset
+
+ds = AMReXDataset('plt00001')
+
+sl = yt.SlicePlot(ds, 2, 'Ex', aspect=1)
+sl.annotate_particles(width=(1., 'm'), p_size=2, ptype='beam', col='black')
+sl.save('./img.png')
