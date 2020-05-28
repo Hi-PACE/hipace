dad3f796932b4e6c1480791565ff37926d6f4087
Remi Lehe <remi.lehe@normalesup.org>
2020-05-28 11:21:27 -0700

    Added function for beam deposition

---
+++ src/particles/deposition/BeamDepositCurrent.cpp
@@ -0,0 +1,13 @@
+#include "particles/BeamParticleContainer.H"
+#include "fields/Fields.H"
+
+void
+DepositCurrent (BeamParticleContainer& beam, Fields& fields)
+{
+
+    for (WarpXParIter pti(*this, lev); pti.isValid(); ++pti)
+    {
+        continue;
+    }
+
+}
