376ef5159980c5027b7d15c187c2aa95a8cf1721
Remi Lehe <remi.lehe@normalesup.org>
2020-05-28 12:20:13 -0700

    Intermediate development for current deposition

--- src/particles/deposition/BeamDepositCurrent.cpp
+++ src/particles/deposition/BeamDepositCurrent.cpp
@@ -2,12 +2,17 @@
 #include "fields/Fields.H"
 
 void
-DepositCurrent (BeamParticleContainer& beam, Fields& fields)
+DepositCurrent (BeamParticleContainer& beam, Fields& fields, const int lev=0)
 {
 
-    for (WarpXParIter pti(*this, lev); pti.isValid(); ++pti)
+    // Loop over particle boxes
+    for (BeamParticleIterator pti(beam, lev); pti.isValid(); ++pti)
     {
-        continue;
+        // Extract corresponding fields for this box
+        amrex::FArrayBox& fields_in_box = fields.getF()[lev][pti];
+
+        // Call deposition function in each box
+        //doChargeDepositionShapeN<2>( pti, fields_in_box );
     }
 
 }
