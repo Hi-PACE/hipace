e912e96e9561eb09d6fb4c623ca79d6fd69de6cb
MaxThevenet <mthevenet@lbl.gov>
2020-07-20 18:49:59 +0200

    typo, and better naming in plasma init function

--- src/particles/PlasmaParticleContainer.cpp
+++ src/particles/PlasmaParticleContainer.cpp
@@ -8,9 +8,8 @@ PlasmaParticleContainer::PlasmaParticleContainer (amrex::AmrCore* amr_core)
     amrex::Vector<amrex::Real> tmp_vector;
     if (pp.queryarr("ppc", tmp_vector)){
         AMREX_ALWAYS_ASSERT_WITH_MESSAGE(tmp_vector.size() == AMREX_SPACEDIM-1,
-            "ppc is only specified in transverse directions for plasma particles, it is 1 in the longitudinal direction z. Hence, in 3D, plasma.ppc should only contain 2 values");
+                                         "ppc is only specified in transverse directions for plasma particles, it is 1 in the longitudinal direction z. Hence, in 3D, plasma.ppc should only contain 2 values");
         for (int i=0; i<AMREX_SPACEDIM-1; i++) m_ppc[i] = tmp_vector[i];
-        m_ppc[2] = 1;
     }
     pp.query("uz_mean", m_uz_mean);
     pp.query("u_std", m_u_std);
