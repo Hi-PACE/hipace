16eb79068bd4cc0b6ad55ab304a742464ac379c6
MaxThevenet <mthevenet@lbl.gov>
2020-07-20 09:28:04 +0200

    Current deposition for plasma particles (#61)
    * better file name
    
    * add current deposition for particle species, and call it on 1 slice

---
+++ src/particles/deposition/PlasmaDepositCurrent.cpp
@@ -0,0 +1,55 @@
+#include "PlasmaDepositCurrent.H"
+
+#include "particles/PlasmaParticleContainer.H"
+#include "particles/deposition/PlasmaDepositCurrentInner.H"
+#include "fields/Fields.H"
+#include "Constants.H"
+#include "Hipace.H"
+
+void
+DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
+                amrex::Geometry const& gm, int const lev)
+{
+
+    // Extract properties associated with physical size of the box
+    amrex::Real const * AMREX_RESTRICT dx = gm.CellSize();
+
+    PhysConst phys_const = get_phys_const();
+
+    // Loop over particle boxes
+    for (PlasmaParticleIterator pti(plasma, lev); pti.isValid(); ++pti)
+    {
+        // Extract properties associated with the extent of the current box
+        amrex::Box tilebox = pti.tilebox().grow(2); // Grow to capture the extent of the particle shape
+
+        amrex::RealBox const grid_box{tilebox, gm.CellSize(), gm.ProbLo()};
+        amrex::Real const * AMREX_RESTRICT xyzmin = grid_box.lo();
+        amrex::Dim3 const lo = amrex::lbound(tilebox);
+
+        // Extract the fields currents
+        amrex::MultiFab& S = fields.getSlices(lev, 1);
+        amrex::MultiFab jx(S, amrex::make_alias, FieldComps::jx, S.nGrow());
+        amrex::MultiFab jy(S, amrex::make_alias, FieldComps::jy, S.nGrow());
+        amrex::MultiFab jz(S, amrex::make_alias, FieldComps::jz, S.nGrow());
+        // Extract FabArray for this box
+        amrex::FArrayBox& jx_fab = jx[pti];
+        amrex::FArrayBox& jy_fab = jy[pti];
+        amrex::FArrayBox& jz_fab = jz[pti];
+
+        // For now: fix the value of the charge
+        amrex::Real q = - phys_const.q_e;
+
+        // Call deposition function in each box
+        if        (Hipace::m_depos_order_xy == 0){
+                doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q );
+        } else if (Hipace::m_depos_order_xy == 1){
+                doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q );
+        } else if (Hipace::m_depos_order_xy == 2){
+                doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q );
+        } else if (Hipace::m_depos_order_xy == 3){
+                doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q );
+        } else {
+            amrex::Abort("unknow deposition order");
+        }
+    }
+}
