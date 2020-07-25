a961ead3c80844868f29e3917d063ecfdbfff1ce
Severin Diederichs <severin.diederichs@desy.de>
2020-07-25 18:55:28 +0200

    modified input and input_normalized to allow for a comparison. Added instructions to analysis.py to plot the normalized results

--- examples/beam_in_vacuum/analysis.py
+++ examples/beam_in_vacuum/analysis.py
@@ -21,18 +21,20 @@ from yt.frontends.boxlib.data_structures import AMReXDataset
 
 ds = AMReXDataset('plt00001')
 
-# Radius of the can beam
-R = 10.e-6
+
 # Density of the can beam
-dens = 1.e3
+dens = 2.8239587008591567e23 # at this density, 1/kp = 10um, allowing for an easy comparison with normalized units
+# Radius of the can beam
+R = 10.e-6 # = 1 in normalized units
+
 
 # Define array for transverse coordinate and theory for By
 x = np.linspace(ds.domain_left_edge[0].v, ds.domain_right_edge[0].v, ds.domain_dimensions[0])
-jz = scc.e * scc.c * dens
-By_th = scc.mu_0 * jz * x / 2.
-By_th[abs(x)>=R] = scc.mu_0 * jz * R**2/(2*x[abs(x)>R])
+jz = scc.e * scc.c * dens # = 1 in normalized units
+By_th = scc.mu_0 * jz * x / 2. # = x / 2. in normalizeed units
+By_th[abs(x)>=R] = scc.mu_0 * jz * R**2/(2*x[abs(x)>R]) # remove scc.mu_0 for normalized units
 
-# Load Hipace data for By
+# Load Hipace data for By in SI units
 all_data_level_0 = ds.covering_grid(level=0, left_edge=ds.domain_left_edge,
     dims=ds.domain_dimensions)
 By_sim = all_data_level_0['By'].v.squeeze()[:,ds.domain_dimensions[1]//2,
@@ -41,13 +43,18 @@ By_sim = all_data_level_0['By'].v.squeeze()[:,ds.domain_dimensions[1]//2,
 # Plot simulation result and theory
 matplotlib.rcParams.update({'font.size': 18})
 plt.figure(figsize=(8,8))
-plt.plot(1.e6*x, By_sim, '+-', label='hipace++')
-plt.plot(1.e6*x, By_th, 'k--', label='theory')
+plt.plot(1.e6*x, By_sim, '+-', label='hipace++') # remove *1e6 for normalized units
+plt.plot(1.e6*x, By_th, 'k--', label='theory') # remove *1e6 for normalized units
 plt.grid()
 plt.legend()
 plt.xlim(-50., 50.)
 plt.xlabel('x (um)')
 plt.ylabel('By (A/m)')
+# for normalized units use
+#plt.xlabel('kp x')
+#plt.ylabel('c By / E0')
+#plt.xlim(-5., 5.)
+
 plt.savefig("beam_in_vacuum.png", bbox_inches="tight")
 
 # Assert small error
