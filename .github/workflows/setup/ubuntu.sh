a874835b79f7bef65c8d58d457ea277a1861ff84
MaxThevenet <mthevenet@lbl.gov>
2020-08-04 16:18:38 +0200

    testing github workflow setup

--- .github/workflows/setup/ubuntu.sh
+++ .github/workflows/setup/ubuntu.sh
@@ -12,7 +12,9 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
     build-essential \
     libfftw3-dev    \
-    g++
+    g++ \
+    python3 \
+    python3-pip
 
 python -m pip install --upgrade pip
 python -m pip install --upgrade matplotlib==3.2.2 numpy scipy yt
