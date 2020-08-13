72a5f113fe6b02ac68382479039859542db35881
Severin Diederichs <65728274+SeverinDiederichs@users.noreply.github.com>
2020-08-13 12:55:42 +0200

    Modified input and input_normalized to allow for a comparison. Added … (#73)
    * modified input and input_normalized to allow for a comparison. Added instructions to analysis.py to plot the normalized results
    
    * reduced resolution in input scripts for faster testing
    
    * analysis script for normalized units
    
    * reset benchmark for beam in vacuum
    
    * added checksum test for normalized units
    
    * unified analysis scripts
    
    * Apply suggestions from code review
    
    minor style edits
    
    Co-authored-by: MaxThevenet <mthevenet@lbl.gov>
    
    Co-authored-by: MaxThevenet <mthevenet@lbl.gov>

--- examples/beam_in_vacuum/analysis.py
+++ examples/beam_in_vacuum/analysis.py
@@ -18,31 +18,50 @@ import matplotlib
 import numpy as np
 import scipy.constants as scc
 from yt.frontends.boxlib.data_structures import AMReXDataset
+import argparse
+
+parser = argparse.ArgumentParser(description='Script to analyze the correctness of the beam in vacuum')
+parser.add_argument('--normalized_units',
+                    dest='norm_units',
+                    action='store_true',
+                    default=False,
+                    help='Run the analysis in normalized units')
+parser.add_argument('--do_plot',
+                    dest='do_plot',
+                    action='store_true',
+                    default=False,
+                    help='Plot figures and save them to file')
+
+args = parser.parse_args()
 
 ds = AMReXDataset('plt00001')
 
-do_plot = False
+if args.norm_units:
+    jz0 = 1.
+    mu_0 = 1.
+    R = 1.
+else:
 
-# Radius of the can beam
-R = 10.e-6
-# Density of the can beam
-dens = 1.e3
-
-# Define array for transverse coordinate and theory for By and Bx
-jz0 = scc.e * scc.c * dens
+    # Density of the can beam
+    dens = 2.8239587008591567e23 # at this density, 1/kp = 10um, allowing for an easy comparison with normalized units
+    # Define array for transverse coordinate and theory for By and Bx
+    jz0 = scc.e * scc.c * dens
+    mu_0 = scc.mu_0
+    # Radius of the can beam
+    R = 10.e-6
 
 x = np.linspace(ds.domain_left_edge[0].v, ds.domain_right_edge[0].v, ds.domain_dimensions[0])
-By_th = scc.mu_0 * jz0 * x / 2.
-By_th[abs(x)>=R] = scc.mu_0 * jz0 * R**2/(2*x[abs(x)>R])
+By_th = mu_0 * jz0 * x / 2.
+By_th[abs(x)>=R] = mu_0 * jz0 * R**2/(2*x[abs(x)>R])
 
 y = np.linspace(ds.domain_left_edge[1].v, ds.domain_right_edge[1].v, ds.domain_dimensions[1])
-Bx_th = -scc.mu_0 * jz0 * y / 2.
-Bx_th[abs(y)>=R] = -scc.mu_0 * jz0 * R**2/(2*y[abs(y)>R])
+Bx_th = -mu_0 * jz0 * y / 2.
+Bx_th[abs(y)>=R] = -mu_0 * jz0 * R**2/(2*y[abs(y)>R])
 
 jz_th = - np.ones_like(x) * jz0
 jz_th[abs(x)>=R] = 0.
 
-# Load Hipace data for By
+# Load Hipace data for By in SI units
 all_data_level_0 = ds.covering_grid(level=0, left_edge=ds.domain_left_edge,
     dims=ds.domain_dimensions)
 Bx_sim = all_data_level_0['Bx'].v.squeeze()[ds.domain_dimensions[1]//2,:,ds.domain_dimensions[2]//2]
@@ -50,36 +69,64 @@ By_sim = all_data_level_0['By'].v.squeeze()[:,ds.domain_dimensions[1]//2,ds.doma
 jz_sim = all_data_level_0['jz'].v.squeeze()[:,ds.domain_dimensions[1]//2,ds.domain_dimensions[2]//2]
 
 # Plot simulation result and theory
-if do_plot:
+if args.do_plot:
     matplotlib.rcParams.update({'font.size': 14})
     plt.figure(figsize=(12,4))
 
-    plt.subplot(131)
-    plt.plot(1.e6*y, Bx_sim, '+-', label='hipace++')
-    plt.plot(1.e6*y, Bx_th, 'k--', label='theory')
-    plt.grid()
-    plt.legend()
-    plt.xlim(-50., 50.)
-    plt.xlabel('y (um)')
-    plt.ylabel('Bx (T)')
+    if not args.norm_units:
+        plt.subplot(131)
+        plt.plot(1.e6*y, Bx_sim, '+-', label='hipace++')
+        plt.plot(1.e6*y, Bx_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-50., 50.)
+        plt.xlabel('y (um)')
+        plt.ylabel('Bx (T)')
 
-    plt.subplot(132)
-    plt.plot(1.e6*x, By_sim, '+-', label='hipace++')
-    plt.plot(1.e6*x, By_th, 'k--', label='theory')
-    plt.grid()
-    plt.legend()
-    plt.xlim(-50., 50.)
-    plt.xlabel('x (um)')
-    plt.ylabel('By (T)')
+        plt.subplot(132)
+        plt.plot(1.e6*x, By_sim, '+-', label='hipace++')
+        plt.plot(1.e6*x, By_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-50., 50.)
+        plt.xlabel('x (um)')
+        plt.ylabel('By (T)')
 
-    plt.subplot(133)
-    plt.plot(1.e6*x, jz_sim, '+-', label='hipace++')
-    plt.plot(1.e6*x, jz_th, 'k--', label='theory')
-    plt.grid()
-    plt.legend()
-    plt.xlim(-50., 50.)
-    plt.xlabel('x (um)')
-    plt.ylabel('jz (A/m2)')
+        plt.subplot(133)
+        plt.plot(1.e6*x, jz_sim, '+-', label='hipace++')
+        plt.plot(1.e6*x, jz_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-50., 50.)
+        plt.xlabel('x (um)')
+        plt.ylabel('jz (A/m2)')
+    else:
+        plt.subplot(131)
+        plt.plot(y, Bx_sim, '+-', label='hipace++')
+        plt.plot(y, Bx_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-5., 5.)
+        plt.xlabel('kp y')
+        plt.ylabel('c Bx / E0')
+
+        plt.subplot(132)
+        plt.plot(x, By_sim, '+-', label='hipace++')
+        plt.plot(x, By_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-5., 5.)
+        plt.xlabel('kp x')
+        plt.ylabel('c By / E0')
+
+        plt.subplot(133)
+        plt.plot(x, jz_sim, '+-', label='hipace++')
+        plt.plot(x, jz_th, 'k--', label='theory')
+        plt.grid()
+        plt.legend()
+        plt.xlim(-5., 5.)
+        plt.xlabel('kp x')
+        plt.ylabel('jz /IA')
 
     plt.tight_layout()
 
---
+++ tests/beam_in_vacuum.normalized.Serial.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+# This file is part of the Hipace test suite.
+# It runs a Hipace simulation for a can beam in vacuum, and compares the result
+# of the simulation to theory.
+
+# Read input parameters
+HIPACE_EXECUTABLE=$1
+HIPACE_SOURCE_DIR=$2
+
+HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/beam_in_vacuum
+HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
+
+# Run the simulation
+$HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized hipace.depos_order_xy=1
+
+# Compare the result with theory
+python $HIPACE_EXAMPLE_DIR/analysis.py --normalized_units
+
+# Compare the results with checksum benchmark
+$HIPACE_TEST_DIR/checksum/checksumAPI.py \
+    --evaluate \
+    --plotfile plt00001 \
+    --test-name beam_in_vacuum.normalized.Serial
