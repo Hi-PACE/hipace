0ce309ad321af68be3c6e4923b91bb5d4be3ef38
Severin Diederichs <65728274+SeverinDiederichs@users.noreply.github.com>
2020-08-21 12:42:52 +0200

    Update plasma force terms (#108)
    * [WIP] updating force terms, not working on GPUs
    
    * [WIP] working on GPU fix for update force terms
    
    * [WIP] for testing purposes only
    
    * work
    
    * cleaning code, adding get position functions
    
    * adding doxygen documentation to UpdateForceTerms
    
    * Apply suggestions from code review
    
    Co-authored-by: MaxThevenet <maxence.thevenet@desy.de>
    
    * improve doxygen documentation
    
    * fixing white space
    
    * improve doxygen documentation
    
    * Apply suggestions from code review
    
    Co-authored-by: MaxThevenet <maxence.thevenet@desy.de>
    
    Co-authored-by: MaxThevenet <maxence.thevenet@desy.de>

--- src/particles/pusher/PlasmaParticlePusher.cpp
+++ src/particles/pusher/PlasmaParticlePusher.cpp
@@ -6,7 +6,7 @@
 #include "fields/Fields.H"
 #include "Constants.H"
 #include "Hipace.H"
-
+#include "GetAndSetPosition.H"
 
 void
 UpdateForcePushParticles (PlasmaParticleContainer& plasma, Fields & fields,
@@ -17,6 +17,7 @@ UpdateForcePushParticles (PlasmaParticleContainer& plasma, Fields & fields,
 
     // Extract properties associated with physical size of the box
     amrex::Real const * AMREX_RESTRICT dx = gm.CellSize();
+    const PhysConst phys_const = get_phys_const();
 
     // Loop over particle boxes
     for (PlasmaParticleIterator pti(plasma, lev); pti.isValid(); ++pti)
@@ -56,26 +57,46 @@ UpdateForcePushParticles (PlasmaParticleContainer& plasma, Fields & fields,
         const amrex::GpuArray<amrex::Real, 3> dx_arr = {dx[0], dx[1], dx[2]};
         const amrex::GpuArray<amrex::Real, 3> xyzmin_arr = {xyzmin[0], xyzmin[1], xyzmin[2]};
 
-        const auto& aos = pti.GetArrayOfStructs(); // For positions
-        const auto& pos_structs = aos.begin();
-        // const auto& soa = pti.GetStructOfArrays(); // For momenta and weights
+        auto& soa = pti.GetStructOfArrays(); // For momenta and weights
+
+        // loading the data
+        amrex::Real * const uxp = soa.GetRealData(PlasmaIdx::ux).data();
+        amrex::Real * const uyp = soa.GetRealData(PlasmaIdx::uy).data();
+        amrex::Real * const psip = soa.GetRealData(PlasmaIdx::psi).data();
+
+        amrex::Real * const Fx1 = soa.GetRealData(PlasmaIdx::Fx1).data();
+        amrex::Real * const Fy1 = soa.GetRealData(PlasmaIdx::Fy1).data();
+        amrex::Real * const Fux1 = soa.GetRealData(PlasmaIdx::Fux1).data();
+        amrex::Real * const Fuy1 = soa.GetRealData(PlasmaIdx::Fuy1).data();
+        amrex::Real * const Fpsi1 = soa.GetRealData(PlasmaIdx::Fpsi1).data();
 
         const int depos_order_xy = Hipace::m_depos_order_xy;
+        const amrex::Real clightsq = 1.0_rt/(phys_const.c*phys_const.c);
+
+        const auto getPosition = GetParticlePosition(pti);
+        const amrex::Real zmin = xyzmin[2];
 
         amrex::ParallelFor(pti.numParticles(),
             [=] AMREX_GPU_DEVICE (long ip) {
 
+                amrex::ParticleReal xp, yp, zp;
+                getPosition(ip, xp, yp, zp);
                 // define field at particle position reals
                 amrex::ParticleReal ExmByp = 0._rt, EypBxp = 0._rt, Ezp = 0._rt;
                 amrex::ParticleReal Bxp = 0._rt, Byp = 0._rt, Bzp = 0._rt;
 
+
+
                 // field gather for a single particle
-                doGatherShapeN(pos_structs[ip].pos(0), pos_structs[ip].pos(1), xyzmin[2],
+                doGatherShapeN(xp, yp, zmin,
                     ExmByp, EypBxp, Ezp, Bxp, Byp, Bzp,
                     exmby_arr, eypbx_arr, ez_arr, bx_arr, by_arr, bz_arr,
                     dx_arr, xyzmin_arr, lo, depos_order_xy, 0);
 
-                // insert update force terms for a single particle
+                // update force terms for a single particle
+                UpdateForceTerms( uxp[ip], uyp[ip], psip[ip], ExmByp, EypBxp, Ezp,
+                                  Bxp, Byp, Bzp, Fx1[ip], Fy1[ip], Fux1[ip], Fuy1[ip],
+                                  Fpsi1[ip], clightsq);
 
                 //insert push a single particle
 
