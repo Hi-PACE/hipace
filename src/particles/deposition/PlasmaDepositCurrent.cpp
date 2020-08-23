148ceb599be3400eded6c5919a9fb842e884aa2d
MaxThevenet <mthevenet@lbl.gov>
2020-08-23 08:12:07 +0200

    replace loop over ParIter by MFIter

--- src/particles/deposition/PlasmaDepositCurrent.cpp
+++ src/particles/deposition/PlasmaDepositCurrent.cpp
@@ -16,41 +16,46 @@ DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
 
     PhysConst phys_const = get_phys_const();
 
+    amrex::MultiFab& S = fields.getSlices(lev, 1);
+
     // Loop over particle boxes
-    for (PlasmaParticleIterator pti(plasma, lev); pti.isValid(); ++pti)
+    for ( amrex::MFIter mfi(S); mfi.isValid(); ++mfi )
     {
         // Extract properties associated with the extent of the current box
-        amrex::Box tilebox = pti.tilebox().grow(2); // Grow to capture the extent of the particle shape
+        // amrex::Box tilebox = pti.tilebox().grow(2); // Grow to capture the extent of the particle shape
+        // Grow to capture the extent of the particle shape
+        //amrex::Box tilebox = mfi.tilebox().grow(
+        //    {Hipace::m_depos_order_xy, Hipace::m_depos_order_xy, 0});
+        amrex::Box tilebox = mfi.tilebox().grow(2);
 
         amrex::RealBox const grid_box{tilebox, gm.CellSize(), gm.ProbLo()};
         amrex::Real const * AMREX_RESTRICT xyzmin = grid_box.lo();
         amrex::Dim3 const lo = amrex::lbound(tilebox);
 
         // Extract the fields currents
-        amrex::MultiFab& S = fields.getSlices(lev, 1);
         amrex::MultiFab jx(S, amrex::make_alias, FieldComps::jx, 1);
         amrex::MultiFab jy(S, amrex::make_alias, FieldComps::jy, 1);
         amrex::MultiFab jz(S, amrex::make_alias, FieldComps::jz, 1);
         // Extract FabArray for this box
-        amrex::FArrayBox& jx_fab = jx[pti];
-        amrex::FArrayBox& jy_fab = jy[pti];
-        amrex::FArrayBox& jz_fab = jz[pti];
+        amrex::FArrayBox& jx_fab = jx[mfi];
+        amrex::FArrayBox& jy_fab = jy[mfi];
+        amrex::FArrayBox& jz_fab = jz[mfi];
 
         // For now: fix the value of the charge
         amrex::Real q = - phys_const.q_e;
 
         // Call deposition function in each box
         if        (Hipace::m_depos_order_xy == 0){
-                doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+            doDepositionShapeN<0, 0>( plasma, mfi, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 1){
-                doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<1, 0>( plasma, mfi, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 2){
-                doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<2, 0>( plasma, mfi, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 3){
-                doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<3, 0>( plasma, mfi, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else {
             amrex::Abort("unknow deposition order");
