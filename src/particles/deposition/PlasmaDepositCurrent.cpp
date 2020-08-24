b0fc1f704110c852e12d1ce5be016fd4c3386416
Severin Diederichs <65728274+SeverinDiederichs@users.noreply.github.com>
2020-08-24 08:20:49 +0200

    Deposit plasma density (#112)
    * added plasma density deposition
    
    * improved description for doxygen
    
    * fix typo
    
    * moving the volume factor to the right position for SI unit charge density deposition
    
    * updating checksum tests, after changing rho for SI units
    
    * updating checksum test results using the reset all script
    
    * reset benchmark for the same number of OMP threads as github
    
    * updated checksum test with new no_acc compilation
    
    * removing 2Rank checksum test due to bugs in the longitudinal parallelization
    
    * Apply suggestions from code review
    
    * eol whitespace
    
    Co-authored-by: MaxThevenet <maxence.thevenet@desy.de>
    Co-authored-by: MaxThevenet <mthevenet@lbl.gov>

--- src/particles/deposition/PlasmaDepositCurrent.cpp
+++ src/particles/deposition/PlasmaDepositCurrent.cpp
@@ -31,26 +31,28 @@ DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
         amrex::MultiFab jx(S, amrex::make_alias, FieldComps::jx, 1);
         amrex::MultiFab jy(S, amrex::make_alias, FieldComps::jy, 1);
         amrex::MultiFab jz(S, amrex::make_alias, FieldComps::jz, 1);
+        amrex::MultiFab rho(S, amrex::make_alias, FieldComps::rho, 1);
         // Extract FabArray for this box
         amrex::FArrayBox& jx_fab = jx[pti];
         amrex::FArrayBox& jy_fab = jy[pti];
         amrex::FArrayBox& jz_fab = jz[pti];
+        amrex::FArrayBox& rho_fab = rho[pti];
 
         // For now: fix the value of the charge
         amrex::Real q = - phys_const.q_e;
 
         // Call deposition function in each box
         if        (Hipace::m_depos_order_xy == 0){
-                doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 1){
-                doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 2){
-                doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else if (Hipace::m_depos_order_xy == 3){
-                doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, dx, xyzmin, lo, q,
+                doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab, dx, xyzmin, lo, q,
                                           CurrentDepoType::DepositThisSlice );
         } else {
             amrex::Abort("unknow deposition order");
