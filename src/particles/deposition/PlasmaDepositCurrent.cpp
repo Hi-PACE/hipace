678f804cd107e7e1d145d958777815f9ff7ebbd4
Severin Diederichs <severin.diederichs@desy.de>
2020-09-07 12:00:21 +0200

    using which_slice directly instead of if (which_slice ==WhichSlice) else ..., thus removing duplicated code

--- src/particles/deposition/PlasmaDepositCurrent.cpp
+++ src/particles/deposition/PlasmaDepositCurrent.cpp
@@ -37,69 +37,38 @@ DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
         amrex::Dim3 const lo = amrex::lbound(tilebox);
 
         // Extract the fields currents
-        amrex::MultiFab& S = fields.getSlices(lev, WhichSlice::This);
-        amrex::MultiFab& next_slice = fields.getSlices(lev, WhichSlice::Next);
+        amrex::MultiFab& S = fields.getSlices(lev, which_slice);
         amrex::MultiFab jx(S, amrex::make_alias, FieldComps::jx, 1);
         amrex::MultiFab jy(S, amrex::make_alias, FieldComps::jy, 1);
-        amrex::MultiFab jx_next(next_slice, amrex::make_alias, FieldComps::jx, 1);
-        amrex::MultiFab jy_next(next_slice, amrex::make_alias, FieldComps::jy, 1);
         amrex::MultiFab jz(S, amrex::make_alias, FieldComps::jz, 1);
         amrex::MultiFab rho(S, amrex::make_alias, FieldComps::rho, 1);
         // Extract FabArray for this box
         amrex::FArrayBox& jx_fab = jx[pti];
         amrex::FArrayBox& jy_fab = jy[pti];
-        amrex::FArrayBox& jx_next_fab = jx_next[pti];
-        amrex::FArrayBox& jy_next_fab = jy_next[pti];
         amrex::FArrayBox& jz_fab = jz[pti];
         amrex::FArrayBox& rho_fab = rho[pti];
 
         // For now: fix the value of the charge
-        amrex::Real q = - phys_const.q_e;
+        amrex::Real q = (which_slice == WhichSlice::RhoIons ) ? phys_const.q_e : - phys_const.q_e;
 
         // Call deposition function in each box
-        if (which_slice == WhichSlice::This)
-        {
-            // Deposit ion charge density, assumed uniform
-            rho.plus(phys_const.q_e * plasma.m_density, 0, 1);
+        // Deposit ion charge density, assumed uniform
+        rho.plus(phys_const.q_e * plasma.m_density, 0, 1);
 
-            if        (Hipace::m_depos_order_xy == 0){
-                    doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
-                                              dx, xyzmin, lo, q, WhichSlice::This );
-            } else if (Hipace::m_depos_order_xy == 1){
-                    doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
-                                              dx, xyzmin, lo, q, WhichSlice::This );
-            } else if (Hipace::m_depos_order_xy == 2){
-                    doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
-                                              dx, xyzmin, lo, q, WhichSlice::This );
-            } else if (Hipace::m_depos_order_xy == 3){
-                    doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
-                                              dx, xyzmin, lo, q, WhichSlice::This );
-            } else {
-                amrex::Abort("unknow deposition order");
-            }
+        if        (Hipace::m_depos_order_xy == 0){
+                doDepositionShapeN<0, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
+                                          dx, xyzmin, lo, q, which_slice );
+        } else if (Hipace::m_depos_order_xy == 1){
+                doDepositionShapeN<1, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
+                                          dx, xyzmin, lo, q, which_slice );
+        } else if (Hipace::m_depos_order_xy == 2){
+                doDepositionShapeN<2, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
+                                          dx, xyzmin, lo, q, which_slice );
+        } else if (Hipace::m_depos_order_xy == 3){
+                doDepositionShapeN<3, 0>( pti, jx_fab, jy_fab, jz_fab, rho_fab,
+                                          dx, xyzmin, lo, q, which_slice );
+        } else {
+            amrex::Abort("unknow deposition order");
         }
-        else if (which_slice == WhichSlice::Next)
-        {
-            if        (Hipace::m_depos_order_xy == 0){
-                    doDepositionShapeN<0, 0>( pti, jx_next_fab, jy_next_fab, jz_fab,
-                                              rho_fab, dx, xyzmin, lo, q,
-                                              WhichSlice::Next );
-            } else if (Hipace::m_depos_order_xy == 1){
-                    doDepositionShapeN<1, 0>( pti, jx_next_fab, jy_next_fab, jz_fab,
-                                              rho_fab, dx, xyzmin, lo, q,
-                                              WhichSlice::Next );
-            } else if (Hipace::m_depos_order_xy == 2){
-                    doDepositionShapeN<2, 0>( pti, jx_next_fab, jy_next_fab, jz_fab,
-                                              rho_fab, dx, xyzmin, lo, q,
-                                              WhichSlice::Next );
-            } else if (Hipace::m_depos_order_xy == 3){
-                    doDepositionShapeN<3, 0>( pti, jx_next_fab, jy_next_fab, jz_fab,
-                                              rho_fab, dx, xyzmin, lo, q,
-                                              WhichSlice::Next );
-            } else {
-                amrex::Abort("unknow deposition order");
-            }
-        }
-
     }
 }
