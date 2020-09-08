c68f6928fc4204f203b0e255c15e1602a15769c1
Severin Diederichs <severin.diederichs@desy.de>
2020-09-08 16:29:11 +0200

    now the real particle position will be updated, when the particles are pushed to the next slice (instead of a temporary position). The previous position needs to be temporarily stored. This allows for an additional redistribute call after the particle push in the next slice, fixing an important error and source of crashes

--- src/Hipace.cpp
+++ src/Hipace.cpp
@@ -344,6 +344,7 @@ Hipace::PredictorCorrectorLoopToSolveBxBy (const amrex::Box& bx, const int islic
         AdvancePlasmaParticles(m_plasma_container, m_fields, geom[lev],
                                WhichSlice::Next,
                                true, false, false, lev);
+        m_plasma_container.Redistribute();
 
         /* deposit current to next slice */
         DepositCurrent(m_plasma_container, m_fields, WhichSlice::Next, geom[lev], lev);
@@ -547,7 +548,7 @@ Hipace::WriteDiagnostics (int step)
         amrex::Vector<int> int_flags(PlasmaIdx::nattribs, 1);
         amrex::Vector<std::string> real_names {
             "w","ux","uy", "psi",
-            "x_temp", "y_temp", "w_temp", "ux_temp", "uy_temp", "psi_temp",
+            "x_prev", "y_prev", "w_temp", "ux_temp", "uy_temp", "psi_temp",
             "Fx1", "Fx2", "Fx3", "Fx4", "Fx5",
             "Fy1", "Fy2", "Fy3", "Fy4", "Fy5",
             "Fux1", "Fux2", "Fux3", "Fux4", "Fux5",
