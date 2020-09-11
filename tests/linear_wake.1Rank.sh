af88621b4fecf7575061b91f3f912ea828c2eae2
Severin Diederichs <severin.diederichs@desy.de>
2020-09-11 15:05:28 +0200

    adding CI for linear wake vs theory

---
+++ tests/linear_wake.1Rank.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+# This file is part of the Hipace test suite.
+# It runs a Hipace simulation in the linear regime and compares the result
+# with theory.
+
+# Read input parameters
+HIPACE_EXECUTABLE=$1
+HIPACE_SOURCE_DIR=$2
+
+HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/linear_wake
+HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
+
+# Run the simulation
+mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs
+
+# Compare the result with theory
+python3 $HIPACE_EXAMPLE_DIR/analysis.py
+
+# Compare the results with checksum benchmark
+$HIPACE_TEST_DIR/checksum/checksumAPI.py \
+    --evaluate \
+    --plotfile plt00001 \
+    --test-name linear_wake.1Rank
