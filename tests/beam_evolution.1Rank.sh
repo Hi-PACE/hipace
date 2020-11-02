945fea47c5f5b778296cce3c1ecde0aed5481f2f
Severin Diederichs <severin.diederichs@desy.de>
2020-11-02 20:57:03 +0100

    adding CI test for beam evolution

---
+++ tests/beam_evolution.1Rank.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+# This file is part of the Hipace test suite.
+# It runs a Hipace simulation in the blowout regime and compares the result
+# with SI units.
+
+# abort on first encounted error
+set -eu -o pipefail
+
+# Read input parameters
+HIPACE_EXECUTABLE=$1
+HIPACE_SOURCE_DIR=$2
+
+HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/blowout_wake
+HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
+
+# Run the simulation
+mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized \
+            max_step = 3 \
+            hipace.dt = 3.0 \
+            hipace.output_period = 3 \
+
+# Compare the results with checksum benchmark
+$HIPACE_TEST_DIR/checksum/checksumAPI.py \
+    --evaluate \
+    --plotfile plt00003 \
+    --test-name beam_evolution.1Rank
