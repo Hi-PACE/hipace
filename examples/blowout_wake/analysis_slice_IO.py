55a52c29f90751500bbc102179a2ca06a6bcee9d
Maxence Thevenet <maxence.thevenet@desy.de>
2020-11-10 17:32:51 +0100

    update CI for slice IO

--- examples/blowout_wake/analysis_slice_IO.py
+++ examples/blowout_wake/analysis_slice_IO.py
@@ -14,14 +14,40 @@ from yt.frontends.boxlib.data_structures import AMReXDataset
 import math
 import argparse
 
+do_plot = False
+
 ds1 = AMReXDataset('full_io')
 all_data_level_0 = ds1.covering_grid(level=0,
                                      left_edge=ds1.domain_left_edge,
                                      dims=ds1.domain_dimensions)
 Ez_full = all_data_level_0['Ez'].v.squeeze()
+Ez_full_sliced = Ez_full[:,Ez_full.shape[1]//2,:].squeeze()
 
 ds2 = AMReXDataset('slice_io')
-all_data_level_0 = ds2.covering_grid(level=0, left_edge=ds2.domain_left_edge, dims=ds2.domain_dimensions)
-Ez_slice = all_data_level_0['Ez'].v.squeeze()
+ad = ds2.all_data()
+Ez_slice = ad['Ez'].reshape(ds2.domain_dimensions).v.squeeze()
 
-assert(np.all(Ez_slice[:,Ez_slice.shape[1]//2,:] == Ez_full[:,Ez_full.shape[1]//2,:]))
+if do_plot:
+    plt.figure(figsize=(6,12))
+    plt.subplot(131)
+    plt.title('full')
+    plt.imshow(Ez_full_sliced)
+    plt.colorbar()
+    plt.subplot(132)
+    plt.title('slice')
+    plt.imshow(Ez_slice)
+    plt.colorbar()
+    plt.subplot(133)
+    plt.title('difference')
+    plt.imshow(Ez_slice-Ez_full_sliced)
+    plt.colorbar()
+    plt.tight_layout()
+    plt.savefig("image.pdf", bbox_inches='tight')
+
+error = np.max(np.abs(Ez_slice-Ez_full_sliced)) / np.max(np.abs(Ez_full_sliced))
+
+print("Ez_full.shape", Ez_full.shape)
+print("Ez_slice.shape", Ez_slice.shape)
+print("error", error)
+
+assert(np.all(Ez_slice == Ez_full[:,Ez_full.shape[1]//2,:]))
