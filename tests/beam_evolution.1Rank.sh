2e358011c79ebfba9ed49e4230bd9935d9fc7ce4
Maxence Thevenet <maxence.thevenet@desy.de>
2020-11-12 13:08:48 +0100

    add analytical test for beam pusher, using external fields

--- tests/beam_evolution.1Rank.sh
+++ tests/beam_evolution.1Rank.sh
@@ -11,19 +11,29 @@ set -eu -o pipefail
 HIPACE_EXECUTABLE=$1
 HIPACE_SOURCE_DIR=$2
 
-HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/blowout_wake
+HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/beam_in_vacuum
 HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
 
 # Run the simulation
 mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized \
-            max_step = 3 \
-            hipace.slice_beam = 1 \
-            hipace.3d_on_host = 1 \
-            hipace.dt = 3.0 \
-            hipace.output_period = 3 \
+        amr.n_cell = 32 32 10 \
+        max_step = 20 \
+        geometry.prob_lo = -2. -2. -2. \
+        geometry.prob_hi =  2.  2.  2. \
+        hipace.slice_beam = 1 \
+        hipace.3d_on_host = 1 \
+        hipace.dt = 3. \
+        hipace.output_period = 20 \
+        beam.density = 1.e-8 \
+        beam.radius = 1. \
+        beam.ppc = 4 4 1 \
+        hipace.external_field_strength = .5
+
+# Compare the result with theory
+$HIPACE_EXAMPLE_DIR/analysis_beam_push.py
 
 # Compare the results with checksum benchmark
 $HIPACE_TEST_DIR/checksum/checksumAPI.py \
     --evaluate \
-    --plotfile plt00003 \
+    --plotfile plt00020 \
     --test-name beam_evolution.1Rank
