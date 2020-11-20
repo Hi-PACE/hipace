19d79618a7ca62d0a228412c07e2210fb2b82204
Severin Diederichs <severin.diederichs@desy.de>
2020-11-20 19:31:20 +0100

    CI for fixed weight beam in normalized units a fixed density

--- tests/gaussian_weight.1Rank.sh
+++ tests/gaussian_weight.1Rank.sh
@@ -15,6 +15,12 @@ HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/gaussian_weight
 HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
 
 # Run the simulation
+mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized
+
+# Compare the result with theory
+$HIPACE_EXAMPLE_DIR/analysis.py --normalized-units
+
+# Run the simulation
 mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_SI
 
 # Compare the result with theory
