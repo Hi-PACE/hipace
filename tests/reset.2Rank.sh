08e635db411bb44e54e082ffe6e2cd82bb9eea1d
Maxence Thevenet <maxence.thevenet@desy.de>
2020-12-11 10:51:56 +0100

    reset test should use 2 ranks too

--- tests/reset.2Rank.sh
+++ tests/reset.2Rank.sh
@@ -15,10 +15,10 @@ HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/blowout_wake
 HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
 
 # Run the simulation
-mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized max_step=2
+mpiexec -n 2 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_normalized max_step=2
 
 # Compare the results with checksum benchmark
 $HIPACE_TEST_DIR/checksum/checksumAPI.py \
     --evaluate \
-    --plotfile plt00001 \
-    --test-name blowout_wake.1Rank
+    --plotfile plt00002 \
+    --test-name blowout_wake.2Rank
