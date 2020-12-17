1db74beedc376061d9c6577f8970cc1db2a94bb9
Axel Huebl <axel.huebl@plasma.ninja>
2020-12-17 09:31:07 -0800

    openPMD: Collective Open & Group Based
    Re-introduce writing to one file to avoid syncs.
    
    Write in parallel into one file, which we open collectively.
    This has the nice side-effect, that later iteration flushes
    can be performed as non-collective writes.

--- src/Hipace.cpp
+++ src/Hipace.cpp
@@ -900,8 +900,7 @@ Hipace::InitDiagnostics ()
     HIPACE_PROFILE("Hipace::InitDiagnostics()");
 
 #ifdef HIPACE_USE_OPENPMD
-    std::string filename = "diags/openPMD/openpmd_%06T.h5"; // bp or h5
-    // std::string filename = "diags/openPMD/openpmd.h5"; // writing to single file did not work
+    std::string filename = "diags/openPMD/openpmd.h5"; // bp or h5
 #   ifdef AMREX_USE_MPI
     m_outputSeries = std::make_unique< io::Series >(
         filename, io::Access::CREATE, amrex::ParallelDescriptor::Communicator());
@@ -910,9 +909,9 @@ Hipace::InitDiagnostics ()
         filename, io::Access::CREATE);
 #   endif
 
-    // // open files early and collectively, so later flush calls are non-collective
-    // m_outputSeries->setIterationEncoding( io::IterationEncoding::groupBased );
-    // m_outputSeries->flush();
+    // open files early and collectively, so later flush calls are non-collective
+    m_outputSeries->setIterationEncoding( io::IterationEncoding::groupBased );
+    m_outputSeries->flush();
 
     // TODO: meta-data: author, mesh path, extensions, software
 #endif
