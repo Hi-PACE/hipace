039b110b84d5781855a618238eee3eae049fa4bb
AlexanderSinn <64009254+AlexanderSinn@users.noreply.github.com>
2021-01-19 22:10:57 +0100

    Readin from HiPACE-C files improved + conversion script (#315)
    * improved conversion script
    
    * automatically hand over beam plasma density
    
    * added documentation to beam conversion script
    
    * added documentation to beam conversion script
    
    Co-authored-by: AlexanderSinn <alexander.sinn@desy.de>

--- src/particles/BeamParticleContainerInit.cpp
+++ src/particles/BeamParticleContainerInit.cpp
@@ -288,7 +288,7 @@ InitBeamFromFileHelper (std::string input_file,
                         bool coordinates_specified,
                         amrex::Array<std::string, AMREX_SPACEDIM> file_coordinates_xyz,
                         const amrex::Geometry& geom,
-                        const amrex::Real n_0)
+                        amrex::Real n_0)
 {
     HIPACE_PROFILE("BeamParticleContainer::InitParticles");
 
@@ -329,7 +329,7 @@ InitBeamFromFile (std::string input_file,
                   bool coordinates_specified,
                   amrex::Array<std::string, AMREX_SPACEDIM> file_coordinates_xyz,
                   const amrex::Geometry& geom,
-                  const amrex::Real n_0)
+                  amrex::Real n_0)
 {
     HIPACE_PROFILE("BeamParticleContainer::InitParticles");
 
@@ -455,7 +455,17 @@ InitBeamFromFile (std::string input_file,
     const PhysConst phys_const_SI = make_constants_SI();
     input_type si_to_norm_pos = (input_type)( 1. );
     input_type si_to_norm_charge = (input_type)( phys_const_SI.q_e );
+
     if(Hipace::m_normalized_units) {
+        if(n_0 == 0) {
+            if(electrons.containsAttribute("Hipace++_Plasma_Density")) {
+                n_0 = electrons.getAttribute("Hipace++_Plasma_Density").get<double>();
+            }
+            else {
+                amrex::Abort("Please specify the plasma density of the external beam "
+                             "to use it with normalized units with beam.plasma_density");
+            }
+        }
         auto dx = geom.CellSizeArray();
         double omega_p = (double)phys_const_SI.q_e * sqrt( (double)n_0 /
                                       ( (double)phys_const_SI.ep0 * (double)phys_const_SI.m_e ) );
@@ -484,7 +494,7 @@ InitBeamFromFile (std::string input_file,
         input_type file_e_m = q_q_data.get()[0] * unit_qq / (q_q_data.get()[0] * unit_mm);
         if( std::abs(file_e_m - ( phys_const_SI.q_e / phys_const_SI.m_e ) ) > 1e9) {
             amrex::Abort("Charge / Mass of Beam Particle from file "
-                         "dose not match electrons (1.7588e11)\n");
+                         "does not match electrons (1.7588e11)\n");
         }
     }
     else {
