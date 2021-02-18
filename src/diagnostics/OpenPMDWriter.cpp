e3c47a37c3a4277f77fa40938feb5fd7e3ecca5c
Maxence Thevenet <maxence.thevenet@desy.de>
2021-02-18 14:34:39 +0100

    clean up default IO path

--- src/diagnostics/OpenPMDWriter.cpp
+++ src/diagnostics/OpenPMDWriter.cpp
@@ -19,7 +19,7 @@ OpenPMDWriter::InitDiagnostics ()
 {
     HIPACE_PROFILE("OpenPMDWriter::InitDiagnostics()");
 
-    std::string filename = m_file_prefix + "openpmd_%06T.h5"; // bp or h5
+    std::string filename = m_file_prefix + "/openpmd_%06T.h5"; // bp or h5
     m_outputSeries = std::make_unique< openPMD::Series >(
         filename, openPMD::Access::CREATE);
 
