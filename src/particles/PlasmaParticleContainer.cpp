c8888d0764e7116c581c67aa53175954dee5e0aa
Maxence Thevenet <maxence.thevenet@desy.de>
2021-03-24 16:25:21 +0100

    default is SI

--- src/particles/PlasmaParticleContainer.cpp
+++ src/particles/PlasmaParticleContainer.cpp
@@ -10,7 +10,7 @@ namespace
         // locally. We cannot use Hipace::m_phys_const as it has not been initialized when the
         // PlasmaParticleContainer constructor is called.
         amrex::ParmParse pph("hipace");
-        bool normalized_units;
+        bool normalized_units = false;
         pph.query("normalized_units", normalized_units);
         PhysConst phys_const = normalized_units ? make_constants_normalized() : make_constants_SI();
 
