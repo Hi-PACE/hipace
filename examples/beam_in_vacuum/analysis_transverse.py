70b2fa54807060502b9b47e18deca7a31717efd4
Maxence Thevenet <maxence.thevenet@desy.de>
2021-05-01 10:08:02 +0200

    add automated test for parallel transverse currents

---
+++ examples/beam_in_vacuum/analysis_transverse.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+# This Python analysis script is part of the code Hipace
+#
+# It compares the transverse field By from a serial and a parallel simulation
+# with transverse beam currents and asserts that the results are the same
+
+import matplotlib.pyplot as plt
+import scipy.constants as scc
+import matplotlib
+import sys
+import numpy as np
+import math
+import argparse
+from openpmd_viewer import OpenPMDTimeSeries
+
+parser = argparse.ArgumentParser(
+    description='Script to analyze the correctness of beam with transverse current')
+parser.add_argument('--serial',
+                    dest='serial',
+                    required=True)
+parser.add_argument('--parallel',
+                    dest='parallel',
+                    required=True)
+parser.add_argument('--do-plot',
+                    dest='do_plot',
+                    action='store_true',
+                    default=False,
+                    help='Plot figures and save them to file')
+args = parser.parse_args()
+
+# Replace the string below, to point to your data
+tss = OpenPMDTimeSeries(args.serial)
+tsp = OpenPMDTimeSeries(args.parallel)
+
+iteration = 8
+field = 'Bx'
+Fs, ms = tss.get_field(iteration=iteration, field=field)
+Fp, mp = tsp.get_field(iteration=iteration, field=field)
+
+error = np.sum((Fp-Fs)**2) / np.sum(Fs**2)
+
+print('error = np.sum((Fp-Fs)**2) / np.sum(Fs**2) = ' +str(error))
+assert(error<1.e-10)
