8f37c1229a5f7edec8381405243eb6148a58d9cb
Maxence Thevenet <maxence.thevenet@desy.de>
2021-07-02 14:14:12 +0200

    add a few missing files

---
+++ src/particles/SliceSort.cpp
@@ -0,0 +1,39 @@
+#include "SliceSort.H"
+
+#include <AMReX_ParticleTransformation.H>
+
+BeamBins
+findParticlesInEachSlice (
+    int /*lev*/, int ibox, amrex::Box bx,
+    BeamParticleContainer& beam, const amrex::Geometry& geom,
+    const BoxSorter& a_box_sorter)
+{
+    // Slice box: only 1 cell transversally, same as bx longitudinally.
+    const amrex::Box cbx ({0,0,bx.smallEnd(2)}, {0,0,bx.bigEnd(2)});
+
+    const int np = a_box_sorter.boxCountsPtr()[ibox];
+    const int offset = a_box_sorter.boxOffsetsPtr()[ibox];
+
+    // Extract particle structures for this tile
+    BeamParticleContainer::ParticleType const* particle_ptr = beam.GetArrayOfStructs()().data();
+    particle_ptr += offset;
+
+    // Extract box properties
+    const auto lo = lbound(cbx);
+    const auto dxi = geom.InvCellSizeArray();
+    const auto plo = geom.ProbLoArray();
+
+    // Find the particles that are in each slice and return collections of indices per slice.
+    BeamBins bins;
+    bins.build(
+        np, particle_ptr, cbx,
+        // Pass lambda function that returns the slice index
+        [=] AMREX_GPU_HOST_DEVICE (const BeamParticleContainer::ParticleType& p)
+        noexcept -> amrex::IntVect
+        {
+            return amrex::IntVect(
+                AMREX_D_DECL(0, 0, static_cast<int>((p.pos(2)-plo[2])*dxi[2]-lo.z)));
+        });
+
+    return bins;
+}
---
+++ src/particles/TileSort.cpp
@@ -0,0 +1,48 @@
+#include "TileSort.H"
+
+#include <AMReX_ParticleTransformation.H>
+
+PlasmaBins
+findParticlesInEachTile (
+    int lev, amrex::Box bx, int bin_size,
+    PlasmaParticleContainer& plasma, const amrex::Geometry& geom)
+{
+    // Tile box: only 1 cell longitudinally, same as bx transversally.
+    const amrex::Box tcbx = bx.coarsen(bin_size);
+    const amrex::Box cbx = {{tcbx.smallEnd(0),tcbx.smallEnd(1),0}, {tcbx.bigEnd(0),tcbx.bigEnd(1),0}};
+    amrex::Print()<<"cbx\n"<<cbx<<'\n';
+
+    PlasmaBins bins;
+
+    // const int np = numParticles();
+    // Extract particle structures for this tile
+    // PlasmaParticleContainer::ParticleType const* particle_ptr = plasma.GetArrayOfStructs()().data();
+    int count = 0;
+    for (PlasmaParticleIterator pti(plasma, lev); pti.isValid(); ++pti) {
+        count += 1;
+
+        // Extract box properties
+        const auto lo = lbound(cbx);
+        const auto dxi = amrex::GpuArray<amrex::Real,AMREX_SPACEDIM>({
+                geom.InvCellSizeArray()[0]/bin_size,
+                geom.InvCellSizeArray()[1]/bin_size,
+                1.});
+        const auto plo = geom.ProbLoArray();
+
+        // Find the particles that are in each tile and return collections of indices per tile.
+        bins.build(
+            pti.numParticles(), pti.GetArrayOfStructs().begin(), cbx,
+            // Pass lambda function that returns the tile index
+            [=] AMREX_GPU_HOST_DEVICE (const PlasmaParticleContainer::ParticleType& p)
+            noexcept -> amrex::IntVect
+            {
+                return amrex::IntVect(
+                    AMREX_D_DECL(
+                        static_cast<int>((p.pos(0)-plo[0])*dxi[0]-lo.x),
+                        static_cast<int>((p.pos(1)-plo[1])*dxi[1]-lo.y),
+                        0));
+            });
+    }
+    AMREX_ALWAYS_ASSERT(count == 1);
+    return bins;
+}
