e307d79d9187192cb97165fad9b4b15d68933b7e
Tim Wetzel <tim.wetzel@desy.de>
2021-08-11 10:18:11 -0700

    Update WrapRocDST with Tim's Impl.
    Co-authored-by: Maxence Thevenet <maxence.thevenet@desy.de>

--- src/fields/fft_poisson_solver/fft/WrapRocDST.cpp
+++ src/fields/fft_poisson_solver/fft/WrapRocDST.cpp
@@ -93,21 +93,98 @@ namespace AnyDST
                         amrex::FArrayBox* fourier_array)
     {
         HIPACE_PROFILE("AnyDST::CreatePlan()");
-        /* todo */
+        DSTplan dst_plan;
+        const int nx = real_size[0];
+        const int ny = real_size[1];
+        rocfft_status status;
+        int dim = 2;
+
+        // Allocate expanded_position_array Real of size (2*nx+2, 2*ny+2)
+        // Allocate expanded_fourier_array Complex of size (nx+2, 2*ny+2)
+        amrex::Box expanded_position_box {{0, 0, 0}, {2*nx+1, 2*ny+1, 0}};
+        amrex::Box expanded_fourier_box {{0, 0, 0}, {nx+1, 2*ny+1, 0}};
+        dst_plan.m_expanded_position_array =
+            std::make_unique<amrex::FArrayBox>(
+                expanded_position_box, 1);
+        dst_plan.m_expanded_fourier_array =
+            std::make_unique<amrex::BaseFab<amrex::GpuComplex<amrex::Real>>>(
+                expanded_fourier_box, 1);
+
+        // setting the initial values to 0
+        // we don't set the expanded Fourier array, because it will be initialized by the FFT
+        dst_plan.m_expanded_position_array->setVal<amrex::RunOn::Device>(0.,
+            dst_plan.m_expanded_position_array->box(), 0,
+            dst_plan.m_expanded_position_array->nComp());
+
+        // check for type of expanded size, should be const size_t *
+        const amrex::IntVect& expanded_size = expanded_position_box.length();
+        const std::size_t lengths[] = {AMREX_D_DECL(std::size_t(expanded_size[0]),
+                                                    std::size_t(expanded_size[1]),
+                                                    std::size_t(expanded_size[2]))};
+
+        // Initialize fft_plan.m_plan with the vendor fft plan.
+        rocfft_status result;
+        result = rocfft_plan_create(&(dst_plan.m_plan), \
+                                    rocfft_placement_notinplace, \
+                                    rocfft_transform_type_real_forward, \
+                                    precision, \
+                                    dim, \
+                                    lengths, \
+                                    1, \
+                                    nullptr);
+
+        assert_rocfft_status("rocfft_plan_create", result);
+
+        // Store meta-data in dst_plan
+        dst_plan.m_position_array = position_array;
+        dst_plan.m_fourier_array = fourier_array;
+
+        return dst_plan;
     }
 
     void DestroyPlan (DSTplan& dst_plan)
     {
-        /* todo */
+        rocfft_plan_destroy( dst_plan.m_plan );
     }
 
-    template<direction d>
+    //template<direction d>
     void Execute (DSTplan& dst_plan){
         HIPACE_PROFILE("AnyDST::Execute()");
 
-        /* todo */
+        // Expand in position space m_position_array -> m_expanded_position_array
+        ExpandR2R(*dst_plan.m_expanded_position_array, *dst_plan.m_position_array);
+
+        rocfft_status result;
+
+        rocfft_execution_info execinfo = NULL;
+        result = rocfft_execution_info_create(&execinfo);
+        assert_rocfft_status("rocfft_execution_info_create", result);
+
+        std::size_t buffersize = 0;
+        result = rocfft_plan_get_work_buffer_size(dst_plan.m_plan, &buffersize);
+        assert_rocfft_status("rocfft_plan_get_work_buffer_size", result);
+
+        result = rocfft_execution_info_set_stream(execinfo, amrex::Gpu::gpuStream());
+        assert_rocfft_status("rocfft_execution_info_set_stream", result);
+
+        // R2C FFT m_expanded_position_array -> m_expanded_fourier_array
+        // 2nd argument type still wrong, should be void*
+        // reinterpret_cast<AnyFFT::Complex*>(dst_plan.m_expanded_fourier_array->dataPtr()), //3rd arg
+        result = rocfft_execute(dst_plan.m_plan, \
+                                (void**)&(dst_plan.m_expanded_position_array), \
+                                (void**)&(dst_plan.m_expanded_fourier_array), \
+                                execinfo);
+
+        assert_rocfft_status("rocfft_execute", result);
+
+        result = rocfft_execution_info_destroy(execinfo);
+        assert_rocfft_status("rocfft_execution_info_destroy", result);
+
+        // Shrink in Fourier space m_expanded_fourier_array -> m_fourier_array
+        ShrinkC2R(*dst_plan.m_fourier_array, *dst_plan.m_expanded_fourier_array);
+
     }
 
-    template void Execute<direction::forward>(DSTplan& dst_plan);
-    template void Execute<direction::backward>(DSTplan& dst_plan);
+    //template void Execute<direction::forward>(DSTplan& dst_plan);
+    //template void Execute<direction::backward>(DSTplan& dst_plan);
 }
