a760cffab7fe39037cbfd0f6553c0776f4387f5c
AlexanderSinn <alexander.sinn@desy.de>
2021-10-20 14:15:47 +0200

    use openPMD standard naming

--- tools/write_beam.py
+++ tools/write_beam.py
@@ -14,7 +14,7 @@ beam_u_std = [0, 0, 0]
 
 kp_inv = constants.c / constants.e * math.sqrt(constants.epsilon_0 * constants.m_e / plasma_density)
 
-single_charge = (beam_density * beam_position_std[0] * beam_position_std[1] *
+single_weight = (beam_density * beam_position_std[0] * beam_position_std[1] *
                  beam_position_std[2] * np.sqrt(2. * math.pi)**3 / n)
 
 np.random.seed(0)
@@ -34,41 +34,42 @@ particle.set_attribute("HiPACE++_Plasma_Density", plasma_density)
 
 dataset = io.Dataset(data[0].dtype,data[0].shape)
 
-particle["r"].unit_dimension = {
+particle["position"].unit_dimension = {
     io.Unit_Dimension.L:  1,
 }
 
-particle["u"].unit_dimension = {
+particle["momentum"].unit_dimension = {
+    io.Unit_Dimension.M:  1,
     io.Unit_Dimension.L:  1,
     io.Unit_Dimension.T: -1,
 }
 
-particle["q"].unit_dimension = {
+particle["charge"].unit_dimension = {
     io.Unit_Dimension.I:  1,
     io.Unit_Dimension.T:  1,
 }
 
-particle["m"].unit_dimension = {
+particle["mass"].unit_dimension = {
     io.Unit_Dimension.M:  1,
 }
 
 for k,m in [["x",0],["y",1],["z",2]]:
-    particle["r"][k].reset_dataset(dataset)
-    particle["r"][k].store_chunk(data[m])
-    particle["r"][k].unit_SI = kp_inv
+    particle["position"][k].reset_dataset(dataset)
+    particle["position"][k].store_chunk(data[m])
+    particle["position"][k].unit_SI = kp_inv
 
 for k,m in [["x",3],["y",4],["z",5]]:
-    particle["u"][k].reset_dataset(dataset)
-    particle["u"][k].store_chunk(data[m])
-    particle["u"][k].unit_SI = 1
+    particle["momentum"][k].reset_dataset(dataset)
+    particle["momentum"][k].store_chunk(data[m])
+    particle["momentum"][k].unit_SI = constants.m_e * constants.c
 
-particle["q"]["q"].reset_dataset(dataset)
-particle["q"]["q"].make_constant(single_charge)
-particle["q"]["q"].unit_SI = constants.e * plasma_density * kp_inv**3
+particle["charge"]["charge"].reset_dataset(dataset)
+particle["charge"]["charge"].make_constant(single_weight)
+particle["charge"]["charge"].unit_SI = constants.e * plasma_density * kp_inv**3
 
-particle["m"]["m"].reset_dataset(dataset)
-particle["m"]["m"].make_constant(single_charge)
-particle["m"]["m"].unit_SI = constants.m_e * plasma_density * kp_inv**3
+particle["mass"]["mass"].reset_dataset(dataset)
+particle["mass"]["mass"].make_constant(single_weight)
+particle["mass"]["mass"].unit_SI = constants.m_e * plasma_density * kp_inv**3
 
 series.flush()
 
