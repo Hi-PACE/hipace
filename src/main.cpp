3c7748e16307a72ce249289166180f09fa7a1500
Weiqun Zhang <WeiqunZhang@lbl.gov>
2021-12-15 00:58:35 -0800

    Fix the scope of profiler for SYCL (#633)
    In main.cpp, the destructor of the profiler was called after
    amrex::Finalize.  This would cause an error in SYCL due to a device
    synchronization call in the dtor, because the SYCL queues in amrex had been
    deleted.  In this commit, we limit the scope of the profiler so that its
    destructor is called before the queues are deleted.  Note that it was never
    an issue for CUDA/HIP, because the device synchronization calls in those
    backends do not need any amrex objects.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@
 int main (int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
-    HIPACE_PROFILE_VAR("main()", pmain);
     {
+        HIPACE_PROFILE_VAR("main()", pmain);
         Hipace hipace;
         hipace.InitData();
         hipace.Evolve();
+        HIPACE_PROFILE_VAR_STOP(pmain);
     }
-    HIPACE_PROFILE_VAR_STOP(pmain);
     amrex::Finalize();
 }
