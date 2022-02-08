12e3f7eaa00cf74db0b81b22eea4ee418920d956
Maxence Thevenet <maxence.thevenet@desy.de>
2022-02-08 14:41:23 +0100

    update for release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'HiPACE++'
 copyright = '2021, Severin Diederichs, Axel Huebl, Remi Lehe, Andrew Myers, Alexander Sinn, Maxence Thevenet, Weiqun Zhang'
 author = 'Severin Diederichs, Axel Huebl, Remi Lehe, Andrew Myers, Alexander Sinn, Maxence Thevenet, Weiqun Zhang'
-version = u'22.01'
-release = u'22.01'
+version = u'22.02'
+release = u'22.02'
 
 # -- General configuration ---------------------------------------------------
 
