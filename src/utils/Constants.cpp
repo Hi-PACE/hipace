ee3ecfea50e9e714453eaf675ff30d3a363ff46c
AlexanderSinn <alexander.sinn@desy.de>
2022-02-22 10:28:31 +0100

    test CI

--- src/utils/Constants.cpp
+++ src/utils/Constants.cpp
@@ -6,3 +6,17 @@ PhysConst get_phys_const ()
     Hipace& hipace = Hipace::GetInstance();
     return hipace.get_phys_const ();
 }
+
+
+namespace Parser {
+    // Cache for evaluated constants
+    std::map<std::string, double> my_constants_cache{};
+
+    // Physical / Numerical Constants available to parsed expressions
+    std::map<std::string, double> hipace_constants
+        {
+            {"pi", MathConst::pi},
+            {"true", 1},
+            {"false", 0}
+        };
+}
