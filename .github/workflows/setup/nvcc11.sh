76af07889e2de59dd464cc559a50fe52c3f541a3
Severin Diederichs <severin.diederichs@desy.de>
2022-07-07 14:49:14 +0200

    update cuda to 11.5

--- .github/workflows/setup/nvcc11.sh
+++ .github/workflows/setup/nvcc11.sh
@@ -18,7 +18,6 @@ set -eu -o pipefail
 sudo apt-get -qqq update
 sudo apt-get install -y \
     build-essential     \
-    g++                 \
     ca-certificates     \
     cmake               \
     gnupg               \
@@ -36,15 +35,15 @@ echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x8
 
 sudo apt-get update
 sudo apt-get install -y          \
-    cuda-command-line-tools-11-4 \
-    cuda-compiler-11-4           \
-    cuda-cupti-dev-11-4          \
-    cuda-minimal-build-11-4      \
-    cuda-nvml-dev-11-4           \
-    cuda-nvtx-11-4               \
-    libcufft-dev-11-4           \
-    libcurand-dev-11-4
-sudo ln -s cuda-11.4 /usr/local/cuda
+    cuda-command-line-tools-11-5 \
+    cuda-compiler-11-5           \
+    cuda-cupti-dev-11-5          \
+    cuda-minimal-build-11-5      \
+    cuda-nvml-dev-11-5           \
+    cuda-nvtx-11-5               \
+    libcufft-dev-11-5            \
+    libcurand-dev-11-5
+sudo ln -s cuda-11.5 /usr/local/cuda
 
 # cmake-easyinstall
 #
