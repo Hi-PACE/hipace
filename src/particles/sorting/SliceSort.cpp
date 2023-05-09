11aa7bdad1892462d6131eddb9eef5dc441bac9b
AlexanderSinn <alexander.sinn@desy.de>
2023-05-10 01:05:30 +0200

    more merge fixes

--- src/particles/sorting/SliceSort.cpp
+++ src/particles/sorting/SliceSort.cpp
@@ -16,16 +16,14 @@ findParticlesInEachSlice (
     [[maybe_unused]] int ibox, [[maybe_unused]] amrex::Box bx, [[maybe_unused]] BeamParticleContainer& beam, [[maybe_unused]] amrex::Geometry const& geom)
 {
     HIPACE_PROFILE("findParticlesInEachSlice()");
-    /*
+
     // Slice box: only 1 cell transversally, same as bx longitudinally.
     amrex::Box cbx ({0,0,bx.smallEnd(2)}, {0,0,bx.bigEnd(2)});
 
     const int np = beam.m_box_sorter.boxCountsPtr()[ibox];
     const int offset = beam.m_box_sorter.boxOffsetsPtr()[ibox];
 
-    // Extract particle structures for this tile
-    BeamParticleContainer::ParticleType const* particle_ptr = beam.GetArrayOfStructs()().data();
-    particle_ptr += offset;
+    AMREX_ALWAYS_ASSERT(offset==0);
 
     // Extract box properties
     const auto lo = lbound(cbx);
@@ -34,12 +32,12 @@ findParticlesInEachSlice (
 
     // Find the particles that are in each slice and return collections of indices per slice.
     beam.m_slice_bins.build(
-        np, particle_ptr, cbx,
+        np, beam.getParticleTileData(), cbx,
         // Pass lambda function that returns the slice index
         [=] AMREX_GPU_DEVICE (const BeamParticleContainer::ParticleType& p)
         noexcept -> amrex::IntVect
         {
             return amrex::IntVect(
                 AMREX_D_DECL(0, 0, static_cast<int>((p.pos(2)-plo[2])*dxi[2]-lo.z)));
-        });*/
+        });
 }
