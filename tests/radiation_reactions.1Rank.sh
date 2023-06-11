d00044a8a1a3262f5566d6e125c30d463847cb4e
Severin <severin.diederichs@desy.de>
2023-06-11 12:56:38 +0200

    increase CI checksum error tolerance due to random beam

--- tests/radiation_reactions.1Rank.sh
+++ tests/radiation_reactions.1Rank.sh
@@ -26,7 +26,8 @@ FILE_NAME=`basename "$0"`
 TEST_NAME="${FILE_NAME%.*}"
 
 # Relative tolerance for checksum tests depends on the platform
-RTOL=1e-12 && [[ "$HIPACE_EXECUTABLE" == *"hipace"*".CUDA."* ]] && RTOL=1e-5
+# high tolerance for CUDA due to random beam
+RTOL=1e-12 && [[ "$HIPACE_EXECUTABLE" == *"hipace"*".CUDA."* ]] && RTOL=1e-2
 
 # Run the simulation
 mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_RR hipace.file_prefix=$TEST_NAME
