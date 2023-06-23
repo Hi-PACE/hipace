78498523cb00e65db95dd11c28a82558d890c379
Severin Diederichs <65728274+SeverinDiederichs@users.noreply.github.com>
2023-06-23 13:39:40 +0200

    break loop over time steps for max_time (#986)
    * break loop over time steps for max_time
    
    * fix EOL

--- src/Hipace.cpp
+++ src/Hipace.cpp
@@ -463,6 +463,8 @@ Hipace::Evolve ()
             WriteDiagnostics(step, it, OpenPMDWriterCallType::fields);
             Notify(step, it);
         }
+        // exit loop over time steps, if max time is exceeded
+        if (m_physical_time > m_max_time) break;
 
         m_multi_beam.InSituWriteToFile(step, m_physical_time, m_3D_geom[0]);
         m_multi_plasma.InSituWriteToFile(step, m_physical_time, m_3D_geom[0]);
