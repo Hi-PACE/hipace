c9a18e7d70e83957e7c35baec9ce7a3f1e2f4abb
AlexanderSinn <alexander.sinn@desy.de>
2023-10-18 14:47:51 +0200

    add more comments

--- src/particles/beam/BeamParticleContainerInit.cpp
+++ src/particles/beam/BeamParticleContainerInit.cpp
@@ -534,6 +534,10 @@ InitBeamFixedWeightPDF3D ()
 
     while (num_added != num_to_add) {
 
+        // It is very unlikely that the correct amount of particles will be initialized in the first
+        // iteration. Another iteration is done with remaining particles (or subtracting extra
+        // particles). This converges quickly as the expected deviation is  sqrt(num_to_add_now),
+        // resulting in a time complexity of O(num_slices*log(log(num_to_add)))
         const amrex::Long num_to_add_now = num_to_add - num_added;
 
         for (int slice=domain.length(2)*m_pdf_ref_ratio-1; slice >=0; --slice) {
