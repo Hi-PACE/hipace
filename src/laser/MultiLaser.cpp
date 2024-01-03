949e19016f58f44cceb5b9609035d79cafb096e5
huixingjian <xingjian.hui@desy.de>
2024-01-03 15:39:37 +0100

    name changed and z-coordinate problem fixed

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -958,7 +958,7 @@ MultiLaser::InitLaserSlice (const amrex::Geometry& geom, const int islice, const
             const auto& laser = m_all_lasers[ilaser];
             const amrex::Real a0 = laser.m_a0;
             const amrex::Real w0 = laser.m_w0;
-            const amrex::Real thetayz0 = laser.m_thetayz0;
+            const amrex::Real init_angle_yz = laser.m_init_angle_yz;
             const amrex::Real x0 = laser.m_position_mean[0];
             const amrex::Real y0 = laser.m_position_mean[1];
             const amrex::Real z0 = laser.m_position_mean[2];
@@ -972,22 +972,23 @@ MultiLaser::InitLaserSlice (const amrex::Geometry& geom, const int islice, const
                     
                     const amrex::Real x = (i+0.5_rt)*dx_arr[0]+plo[0]-x0;
                     const amrex::Real y = (j+0.5_rt)*dx_arr[1]+plo[1]-y0;
-                    const amrex::Real yp=std::cos(thetayz0)*y+std::sin(thetayz0)*z;
-                    const amrex::Real zp=-std::sin(thetayz0)*y+std::cos(thetayz0)*z;
-                    
+                    const amrex::Real yp=std::cos(init_angle_yz)*y+std::sin(init_angle_yz)*z;
+                    const amrex::Real zp=-std::sin(init_angle_yz)*y+std::cos(init_angle_yz)*z;
+                    z=zp;
+                    y=yp;
                     // For first laser, setval to 0.
                     if (ilaser == 0) {
                         arr(i, j, k, comp ) = 0._rt;
                         arr(i, j, k, comp + 1 ) = 0._rt;
                     }
                     // Compute envelope for time step 0
-                    Complex diffract_factor = 1._rt + I * (zp-zfoc*std::cos(thetayz0)) * 2._rt/( k0 * w0 * w0 );
+                    Complex diffract_factor = 1._rt + I * (z-zfoc*std::cos(init_angle_yz)) * 2._rt/( k0 * w0 * w0 );
                     Complex inv_complex_waist_2 = 1._rt /( w0 * w0 * diffract_factor );
                     Complex prefactor = a0/diffract_factor;
-                    Complex time_exponent = zp*zp/(L0*L0);
+                    Complex time_exponent = z*z/(L0*L0);
                     Complex stcfactor = prefactor * amrex::exp( - time_exponent );
-                    Complex exp_argument = - ( x*x + yp*yp ) * inv_complex_waist_2;
-                    Complex envelope = stcfactor * amrex::exp( exp_argument ) * amrex::exp(I * yp * k0 * thetayz0);
+                    Complex exp_argument = - ( x*x + y*y ) * inv_complex_waist_2;
+                    Complex envelope = stcfactor * amrex::exp( exp_argument ) * amrex::exp(I * y * k0 * init_angle_yz);
                     arr(i, j, k, comp ) += envelope.real();
                     arr(i, j, k, comp + 1 ) += envelope.imag();
                 }
