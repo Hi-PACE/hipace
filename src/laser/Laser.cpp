7dbab1b5134b2558177e0a5e702d65522af32858
huixingjian <xingjian.hui@desy.de>
2024-09-06 14:21:21 +0200

    add arguments in Laser object

--- src/laser/Laser.cpp
+++ src/laser/Laser.cpp
@@ -13,13 +13,23 @@
 
 #include <AMReX_Vector.H>
 #include <AMReX_ParmParse.H>
+#include "particles/particles_utils/ShapeFactors.H"
 
-Laser::Laser (std::string name)
+Laser::Laser (std::string name,  amrex::Geometry laser_geom_3D)
 {
     m_name = name;
     amrex::ParmParse pp(m_name);
     queryWithParser(pp, "init_type", m_laser_init_type);
-    if (m_laser_init_type == "from_file") return;
+     if (m_laser_init_type == "from_file") {
+        queryWithParser(pp, "input_file", m_input_file_path);
+        queryWithParser(pp, "openPMD_laser_name", m_file_envelope_name);
+        queryWithParser(pp, "iteration", m_file_num_iteration);
+        if (Hipace::HeadRank()) {
+            m_F_input_file.resize(laser_geom_3D.Domain(), 2, amrex::The_Pinned_Arena());
+            GetEnvelopeFromFileHelper(laser_geom_3D);
+        }
+        return;
+    }
     else if (m_laser_init_type == "gaussian"){
         queryWithParser(pp, "a0", m_a0);
         queryWithParser(pp, "w0", m_w0);
@@ -29,7 +39,7 @@ Laser::Laser (std::string name)
         bool length_is_specified = queryWithParser(pp, "L0", m_L0);
         bool duration_is_specified = queryWithParser(pp, "tau", m_tau);
         AMREX_ALWAYS_ASSERT_WITH_MESSAGE( length_is_specified + duration_is_specified == 1,
-        "Please specify exlusively either the pulse length L0 or the duration tau of the laser");
+       "Please specify exlusively either the pulse length L0 or the duration tau of gaussian lasers");
         if (duration_is_specified) m_L0 = m_tau*get_phys_const().c;
         queryWithParser(pp, "focal_distance", m_focal_distance);
         queryWithParser(pp, "position_mean",  m_position_mean);
@@ -43,5 +53,8 @@ Laser::Laser (std::string name)
         m_profile_real = makeFunctionWithParser<3>( profile_real_str, m_parser_lr, {"x", "y", "z"});
         m_profile_imag = makeFunctionWithParser<3>( profile_imag_str, m_parser_li, {"x", "y", "z"});
         return;
+    }
+    else {
+        amrex::Abort("ilegal init type specified");
     }
 }
