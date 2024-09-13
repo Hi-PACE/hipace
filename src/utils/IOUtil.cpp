db4da8e7bc46794f2cc4d10d2cdd1e8c0efe8989
AlexanderSinn <alexander.sinn@desy.de>
2024-09-13 15:57:50 +0200

    use IOFormatSaver

--- src/utils/IOUtil.cpp
+++ src/utils/IOUtil.cpp
@@ -8,6 +8,7 @@
 #include "IOUtil.H"
 
 #include <AMReX_IndexType.H>
+#include <AMReX_IOFormat.H>
 
 #include <algorithm>
 #include <cmath>
@@ -154,14 +155,14 @@ std::ostream& operator<<(std::ostream& os, utils::format_time ft) {
     long long days = hours / 24;
     hours %= 24;
 
+    amrex::IOFormatSaver iofmtsaver(os);
+
     if (days > 0) {
         os << days << "-";
     }
     os  << std::setfill('0') << std::setw(2) << hours << ":"
         << std::setfill('0') << std::setw(2) << minutes << ":"
         << std::setfill('0') << std::setw(2) << seconds;
-
-    os << std::setfill(' ');
 
     return os;
 }
