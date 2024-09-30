d48b1dad2dd5ba15507374d3a4bc0063bdead4ee
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-09-30 16:32:05 +0200

    Update src/laser/MultiLaser.cpp
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -106,7 +106,7 @@ MultiLaser::MakeLaserGeometry (const amrex::Geometry& field_geom_3D)
 
     for (int i = 0; i < m_nlasers; ++i) {
         m_all_lasers.emplace_back(Laser(m_names[i], m_laser_geom_3D));
-        amrex::Print()<<"laser"+ m_names[i] + "loaded";
+        amrex::Print()<<"Laser "+ m_names[i] + " loaded";
     }
 
     m_slice_box = domain_3D_laser;
