ac709e06e77909eaa7fb98ab9fc126173fcccaeb
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-10 17:27:27 +0100

    Update analysis_laser_init_chirp.py

--- examples/laser/analysis_laser_init_chirp.py
+++ examples/laser/analysis_laser_init_chirp.py
@@ -25,7 +25,7 @@ parser.add_argument('--chirp_type',
                     help='Type of the initialized chirp')
 args = parser.parse_args()
 
-profile = FromOpenPMDProfile(path=args.output_dir,iteration=0,pol=[1,0],field='laserEnvelope', is_envelope=True)
+profile = FromOpenPMDProfile(path=args.output_dir,iteration=0,pol=[1,0],field='laserEnvelope', is_envelope=True，prefix='')
 laser = Laser(
         dim="xyt",
         lo=(-15e-6, -15e-6, -30e-15),
