aa68948e95bf07f6e2f2d2cc851dcd8808abc6b0
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-10 18:03:04 +0100

    Update laser_STC.SI.1Rank.sh

--- tests/laser_STC.SI.1Rank.sh
+++ tests/laser_STC.SI.1Rank.sh
@@ -30,5 +30,5 @@ mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_chirp \
         hipace.file_prefix = $TEST_NAME
 # Compare the result with theory
 $HIPACE_EXAMPLE_DIR/analysis_laser_init_chirp.py --output-dir=$TEST_NAME
-
+ls .
 rm -rf $TEST_NAME
