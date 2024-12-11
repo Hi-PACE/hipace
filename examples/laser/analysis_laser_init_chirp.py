b125be0a814bd035bdefbe9d62c13efd16d7f6a7
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-11 15:51:31 +0100

    Update analysis_laser_init_chirp.py

--- examples/laser/analysis_laser_init_chirp.py
+++ examples/laser/analysis_laser_init_chirp.py
@@ -36,7 +36,7 @@ laser = Laser(
      )
 
 k0 = 2 * scc.pi / 0.6e-6
-stc=get_STC(laser.grid,laser.dim,k0)
+stc=get_STC(xlaser.dim,.6e-6,k0)
 print('zeta is ')
 print(stc['zeta_x'])
 print('beta is ')
