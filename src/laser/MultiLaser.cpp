29388303e810ecbf36965d2222388bc1e32aa400
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-16 19:21:43 +0100

    Update MultiLaser.cpp

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -916,7 +916,7 @@ MultiLaser::InitLaserSlice (const int islice, const int comp)
                         + 2._rt * I * (phi2 - beta * beta * k0 * zfoc) * inv_tau2;
                     Complex prefactor = a0 / diffract_factor;
                     Complex time_exponent = 1._rt / ( stretch_factor * L0 * L0 ) *
-                        amrex::pow(zp + beta * k0 * (x * std::cos(theta_xy) + yp * std::sin(theta_xy)) * clight - 2._rt * I * (x * std::cos(theta_xy) + yp * std::sin(theta_xy))
+                        amrex::pow(zp - beta * k0 * (x * std::cos(theta_xy) + yp * std::sin(theta_xy)) * clight - 2._rt * I * (x * std::cos(theta_xy) + yp * std::sin(theta_xy))
                         * (zeta - beta * zfoc) * clight * inv_complex_waist_2, 2);
                     Complex stcfactor = prefactor * amrex::exp( - time_exponent );
                     Complex exp_argument = - ( x * x + yp * yp ) * inv_complex_waist_2;
