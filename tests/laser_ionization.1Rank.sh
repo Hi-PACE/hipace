1054b336bd66db92aacb8e63671d64b05d0717d9
Eya D. <81635404+EyaDammak@users.noreply.github.com>
2025-01-13 13:34:57 +0100

    Update laser_ionization.1Rank.sh

--- tests/laser_ionization.1Rank.sh
+++ tests/laser_ionization.1Rank.sh
@@ -15,6 +15,7 @@ set -eu -o pipefail
 # Read input parameters
 HIPACE_EXECUTABLE=$1
 HIPACE_SOURCE_DIR=$2
+HIPACE_COMPUTE=$3
 
 HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/laser_ionization
 HIPACE_TEST_DIR=${HIPACE_SOURCE_DIR}/tests
@@ -38,8 +39,10 @@ mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_laser_ionization \
 $HIPACE_EXAMPLE_DIR/analysis_laser_ionization.py --first=$TEST_NAME/linear  --second=$TEST_NAME/circular
 
 
-# Compare the results with checksum benchmark
-$HIPACE_TEST_DIR/checksum/checksumAPI.py \
-    --evaluate \
-    --file_name $TEST_NAME/linear \
-    --test-name $TEST_NAME
+# Compare the results with checksum benchmark if it runs on CPU only
+if [[ "$HIPACE_COMPUTE" != "CUDA" ]]; then
+     $HIPACE_TEST_DIR/checksum/checksumAPI.py \
+         --evaluate \
+         --file_name $TEST_NAME/linear \
+         --test-name $TEST_NAME
+ fi
