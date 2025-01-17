4d60a5da8f835f517b44d213f7d0c74305cd834a
Eya D. <81635404+EyaDammak@users.noreply.github.com>
2025-01-17 11:04:59 +0100

    Update analysis_laser_ionization.py
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- examples/laser_ionization/analysis_laser_ionization.py
+++ examples/laser_ionization/analysis_laser_ionization.py
@@ -37,7 +37,7 @@ iteration = 0
 rho_elec_linear, _ = ts_linear.get_field(field='rho_elec', coord='z', iteration=iteration)
 rho_elec_mean_linear = np.mean(rho_elec_linear, axis=(1, 2))
 rho_average_linear = statistics.mean(rho_elec_mean_linear[0:10])
-fraction_linear = rho_average_linear / (-qe) / (n0)
+fraction_linear = -rho_average_linear / qe / n0
 
 rho_elec_circular, _ = ts_circular.get_field(field='rho_elec', coord='z', iteration=iteration, plot=False)
 rho_elec_mean_circular = np.mean(rho_elec_circular, axis=(1, 2))
