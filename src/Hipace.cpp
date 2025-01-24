9205322f029a1aa655edf1ed28ad8e473e59c9cd
EyaDammak <eya.dammak@ikmail.com>
2025-01-24 16:44:23 +0100

    update hiapce.cpp

--- src/Hipace.cpp
+++ src/Hipace.cpp
@@ -699,7 +699,7 @@ Hipace::SolveOneSlice (int islice, int step)
     m_multi_plasma.DoLaserIonization(islice, m_multi_laser.GetLaserGeom(), m_multi_laser);
 
     // injection
-    m_multi_plasma.DoLaserInjection();
+    m_multi_plasma.DoLaserInjection(MultiBeam& beams);
 
     // Push plasma particles
     for (int lev=0; lev<current_N_level; ++lev) {
