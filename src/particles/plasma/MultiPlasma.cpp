60e53018daf32aefffbca0ed194aeef45f9cad4b
EyaDammak <eya.dammak@ikmail.com>
2025-01-31 17:47:07 +0100

    hello

--- src/particles/plasma/MultiPlasma.cpp
+++ src/particles/plasma/MultiPlasma.cpp
@@ -60,10 +60,10 @@ MultiPlasma::InitData (amrex::Vector<amrex::BoxArray> slice_ba,
         if(plasma.m_can_laser_injection) {
             for (int i=0; i<m_names.size(); ++i) {
                 if(m_names[i] == plasma.m_product_beam_name) {
-                    plasma.m_product_beam_pc = &m_all_beams[i];
+                    plasma.m_product_beam_pc = &m_all_plasmas[i];
                 }
             }
-            AMREX_ALWAYS_ASSERT_WITH_MESSAGE(beam.m_product_beam_pc != nullptr,
+            AMREX_ALWAYS_ASSERT_WITH_MESSAGE(plasma.m_product_beam_pc != nullptr,
                 "Must specify a valid product beam for laser injection using ionization_product");
         }
         
@@ -151,8 +151,7 @@ MultiPlasma::DoLaserInjection (
                  const MultiLaser& laser, const int islice)
 {
     for (auto& plasma : m_all_plasmas) {
-        plasma.PlasmaToBeam(beams, beamnames, laser_geom,
-                 MultiLaser& laser, slice);
+        plasma.PlasmaToBeam(beams, beamnames, laser_geom,laser, islice);
     }
 }
 
