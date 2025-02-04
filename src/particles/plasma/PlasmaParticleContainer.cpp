e9109433618cbd3130ac37a314942370e68a9c5f
Eya D. <81635404+EyaDammak@users.noreply.github.com>
2025-02-04 15:48:58 +0100

    Update PlasmaParticleContainer.cpp

--- src/particles/plasma/PlasmaParticleContainer.cpp
+++ src/particles/plasma/PlasmaParticleContainer.cpp
@@ -672,7 +672,7 @@ LaserIonization (const int islice,
                 if (linear_polarization) {
                     amrex::Real width_p;
                     amrex::Real p_pol;
-                    width_p = std::sqrt(laser_dp_prefactor[ion_lev_loc] * Ep) * std::sqrt(amrex::abs(A*A)); // equation (4) art. Massimo
+                    width_p = std::sqrt(laser_dp_prefactor[ion_lev_loc-1] * Ep) * std::sqrt(amrex::abs(A*A)); // equation (4) art. Massimo
                     p_pol = amrex::RandomNormal(0.0, width_p, engine);
                     ux = p_pol;
                     uy = 0._rt;
