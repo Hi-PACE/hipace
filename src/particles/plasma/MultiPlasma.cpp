2a69eb8c34c5a99569bbfcd1ac5763e9fa53e07d
EyaDammak <eya.dammak@ikmail.com>
2025-02-05 15:03:42 +0100

    new option

--- src/particles/plasma/MultiPlasma.cpp
+++ src/particles/plasma/MultiPlasma.cpp
@@ -99,7 +99,9 @@ MultiPlasma::AdvanceParticles (
     const Fields & fields, amrex::Vector<amrex::Geometry> const& gm, bool temp_slice, int lev)
 {
     for (int i=0; i<m_nplasmas; i++) {
-        AdvancePlasmaParticles(m_all_plasmas[i], fields, gm, temp_slice, lev);
+        if (m_all_plasmas[i].do_push){
+            AdvancePlasmaParticles(m_all_plasmas[i], fields, gm, temp_slice, lev);
+        }
     }
 }
 
