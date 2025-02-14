78504df9283ed7f91a9346c2c4475d11c6d21fa2
Eya D. <81635404+EyaDammak@users.noreply.github.com>
2025-02-14 08:59:14 +0100

    Update analysis_laser_ionization.py

--- examples/laser_ionization/analysis_laser_ionization.py
+++ examples/laser_ionization/analysis_laser_ionization.py
@@ -65,7 +65,7 @@ fraction_warpx_circular = 0.502250841 # result from WarpX simulation
 relative_diff_linear = np.abs( ( fraction_linear - fraction_warpx_linear ) / fraction_warpx_linear )
 relative_diff_circular = np.abs( ( fraction_circular - fraction_warpx_circular ) / fraction_warpx_circular )
 
-tolerance = 0.25
+tolerance = 0.1
 print(f"fraction_warpx_linear = {fraction_warpx_linear}")
 print(f"fraction_hipace_linear = {fraction_linear}")
 print(f"fraction_warpx_circular = {fraction_warpx_circular}")
