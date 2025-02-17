c166f00f5eed96dc9edce3dd139a6ea307f3d2fb
huixingjian <xingjian.hui@desy.de>
2025-02-17 15:40:27 +0100

    fix typo in CI test

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -25,7 +25,7 @@ parser.add_argument('--output-dir',
                     help='Path to the directory containing output files')
 args = parser.parse_args()
 
-profile = FromOpenPMDProfile (path = args.output_dir, iteration = 0, pol=[0,1],field='LaserEnvelope'\
+profile = FromOpenPMDProfile (path = args.output_dir, iteration = 0, pol=[0,1],field='laserEnvelope'\
                               ,coord='', is_envelope=True, prefix='')
 
 laser = Laser(
