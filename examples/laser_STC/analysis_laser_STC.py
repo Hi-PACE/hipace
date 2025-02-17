7b00b2f2aabb5ce680e0ba7f806c5a4a1cbd7cf8
huixingjian <xingjian.hui@desy.de>
2025-02-17 17:02:33 +0100

    phi2 check pass, now check zeta

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -36,6 +36,9 @@ laser = Laser(
         profile=profile,
      )
 
-print(get_phi2(laser.dim, laser.grid))
+
 Phi2,phi2 = get_phi2(laser.dim, laser.grid)
-assert ((phi2-2.4e-24)/2.4e-24<0.01), 'Test laser_ionization did not pass'
+[zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid,k0)
+print([zeta_x, zeta_y])
+#assert ((phi2-2.4e-24)/2.4e-24<0.01), 'Test laser_ionization did not pass'
+assert ((zeta_y-2.4e-22)/2.4e-22<0.01), 'Test zeta did not pass'
