adcfa5fd5c299f819985b54f54142f65a1959e84
huixingjian <xingjian.hui@desy.de>
2025-02-17 17:41:01 +0100

    phi2 and beta passed, now add in zeta and change the resolution in lasy

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -32,14 +32,14 @@ laser = Laser(
         dim="xyt",
         lo=(-15e-6, -15e-6, -30e-15),
         hi=(15e-6,15e-6, +30e-15),
-        npoints=(250, 250, 400),
+        npoints=(127, 127, 200),
         profile=profile,
      )
 
 Phi2,phi2 = get_phi2(laser.dim, laser.grid)
-#[zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid,k0)
-[beta_x, beta_y] = get_beta(laser.dim, laser.grid, 2.0 * np.pi / 0.6e-6)
-print([beta_x, beta_y])
+[beta_x, beta_y] = get_beta(laser.dim, laser.grid, k0)
+[zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid, k0)
+print([zeta_x, zeta_y])
 assert ((phi2-2.4e-24)/2.4e-24<0.01), 'Test phi2 did not pass'
-#assert ((zeta_y-2.4e-22)/2.4e-22<0.01), 'Test zeta did not pass'
+assert ((zeta_y-2.4e-22)/2.4e-22<0.01), 'Test zeta did not pass'
 assert ((beta_y-3e-18)/3e-18<0.01), 'Test zeta did not pass'
