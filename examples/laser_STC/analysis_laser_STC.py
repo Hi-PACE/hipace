6edcf83729469713a929add555dffe60548568c1
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-02-18 17:34:45 +0100

    Update analysis_laser_STC.py

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -37,7 +37,7 @@ laser = Laser(
 Phi2,phi2 = get_phi2(laser.dim, laser.grid)
 [beta_x, beta_y] = get_beta(laser.dim, laser.grid, k0)
 [zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid, k0)
-assert ((phi2-2.4e-24)/2.4e-24<0.01), 'Test phi2 did not pass'
 
+assert ((phi2-2.4e-24)/2.4e-24<0.01), 'Test phi2 did not pass'
 assert ((zeta_y-2.4e-22)/2.4e-22<0.01), 'Test zeta did not pass'
 assert ((beta_y-3e-18)/3e-18<0.01), 'Test zeta did not pass'
