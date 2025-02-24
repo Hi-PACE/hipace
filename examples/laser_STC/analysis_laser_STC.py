1fc9b6a65b2cee8bf59a0f946132eebd856b7695
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-02-25 00:24:02 +0100

    Update analysis_laser_STC.py

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -28,8 +28,8 @@ profile = FromOpenPMDProfile (path = args.output_dir, iteration = 0, pol=[1,0], 
                               ,coord='', is_envelope=True, prefix='openpmd')
 laser = Laser(
         dim="xyt",
-        lo=(-15e-6, -15e-6, -30e-15),
-        hi=(15e-6, 15e-6, +30e-15),
+        lo=(-10e-6, -10e-6, -10e-15),
+        hi=(10e-6, 10e-6, +10e-15),
         npoints=(127, 127, 200),
         profile=profile,
      )
