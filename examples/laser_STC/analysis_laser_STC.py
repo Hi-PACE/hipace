873a06491778b04e8cecaa3fc7aa29ad5b5eab58
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-02-27 22:24:41 +0100

    Finally done!!!!!!!!!

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -10,7 +10,7 @@
 import argparse
 import numpy as np
 import scipy.constants as scc
-from lasy.utils.laser_utils import get_phi2, get_zeta, get_beta, get_duration
+from lasy.utils.laser_utils import get_phi2, get_zeta, get_beta
 from lasy.laser import Laser
 from lasy.profiles import FromOpenPMDProfile
 
@@ -35,10 +35,8 @@ laser = Laser(
      )
 
 Phi2,phi2 = get_phi2(laser.dim, laser.grid)
-tau = get_duration(laser.grid, laser.dim)
 [beta_x, beta_y] = get_beta(laser.dim, laser.grid, k0)
 [zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid, k0)
-print("tau is ",tau)
 print("phi2 theory:", 3e-28, "measured:", phi2)
 print("zeta_y theory:", 2.4e-22, "measured:", zeta_y)
 print("beta_y theory:", 3e-18, "measured:", beta_y)
