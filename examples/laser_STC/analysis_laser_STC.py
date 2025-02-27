36fa5e550c12782c22e1dbf8abbf142455c483f0
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-02-27 22:07:30 +0100

    Update analysis_laser_STC.py

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -39,9 +39,9 @@ tau = get_duration(laser.grid, laser.dim)
 [beta_x, beta_y] = get_beta(laser.dim, laser.grid, k0)
 [zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid, k0)
 print("tau is ",tau)
-print("phi2 theory:", 2.4e-24, "measured:", phi2)
+print("phi2 theory:", 3e-28, "measured:", phi2)
 print("zeta_y theory:", 2.4e-22, "measured:", zeta_y)
 print("beta_y theory:", 3e-18, "measured:", beta_y)
-assert (np.abs((phi2-2.4e-24)/2.4e-24)<0.01), 'Test phi2 did not pass'
-assert (np.abs((zeta_y-2.4e-22)/2.4e-22)<0.2), 'Test zeta did not pass'
-assert (np.abs((beta_y-3e-18)/3e-18)<0.2), 'Test beta did not pass'
+assert (np.abs((phi2-3e-28)/2.4e-24)<0.1), 'Test phi2 did not pass'
+assert (np.abs((zeta_y-2.4e-22)/2.4e-22)<0.1), 'Test zeta did not pass'
+assert (np.abs((beta_y-3e-18)/3e-18)<0.1), 'Test beta did not pass'
