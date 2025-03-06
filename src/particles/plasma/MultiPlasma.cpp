ae75ab93ba7e734b5139fbc1df7d7c3c149b7ed3
EyaDammak <eya.dammak@ikmail.com>
2025-03-06 12:33:00 +0100

    issue 2 in MultiPlasma.cpp

--- src/particles/plasma/MultiPlasma.cpp
+++ src/particles/plasma/MultiPlasma.cpp
@@ -61,7 +61,7 @@ MultiPlasma::InitData (amrex::Vector<amrex::BoxArray> slice_ba,
         }
 
         if(plasma.m_can_laser_injection) {
-            for (int i=0; i<m_names.size(); ++i) {
+            for (int i=0; i<beams.m_names.size(); ++i) {
                 if(beams.m_names[i] == plasma.m_product_beam_name) {
                     plasma.m_product_beam_pc = &beams.m_all_beams[i];
                 }
