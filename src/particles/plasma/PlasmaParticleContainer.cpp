a491d14c82173035839d284d6712bba9557b7856
EyaDammak <eya.dammak@ikmail.com>
2025-03-07 17:01:26 +0100

    ; missing

--- src/particles/plasma/PlasmaParticleContainer.cpp
+++ src/particles/plasma/PlasmaParticleContainer.cpp
@@ -815,8 +815,8 @@ PlasmaToBeam (const MultiLaser& laser, const int islice)
                     amrex::Real psi = ptd_plasma.rdata(PlasmaIdx::psi)[ip];
                     ptd_beam.rdata(BeamIdx::uz)[pidx_beam] = (1+ux*ux+uy*uy-psi*psi)/(2.*psi);
                     ptd_beam.rdata(BeamIdx::w)[pidx_beam] = ptd_plasma.rdata(PlasmaIdx::w)[ip];
-                    ptd_beam.idata(BeamIdx::nsubcycles )[pidx_beam] = 0
-                    ptd_beam.idata(BeamIdx::mr_level )[pidx_beam] = 0;
+                    ptd_beam.idata(BeamIdx::nsubcycles )[pidx_beam] = 0;
+                    ptd_beam.idata(BeamIdx::mr_level)[pidx_beam] = 0;
                 }
             });
             amrex::Gpu::streamSynchronize();
