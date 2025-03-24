9ddbf76fbb7e656bb2725b137f1d69c359ae8cfb
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-03-24 14:33:58 +0100

    Update examples/laser_STC/analysis_laser_STC.py
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- examples/laser_STC/analysis_laser_STC.py
+++ examples/laser_STC/analysis_laser_STC.py
@@ -34,7 +34,7 @@ laser = Laser(
         profile=profile,
      )
 
-Phi2,phi2 = get_phi2(laser.dim, laser.grid)
+Phi2, phi2 = get_phi2(laser.dim, laser.grid)
 [beta_x, beta_y] = get_beta(laser.dim, laser.grid, k0)
 [zeta_x, zeta_y], [nu_x, nu_y] = get_zeta(laser.dim, laser.grid, k0)
 print("phi2 theory:", 3e-28, "measured:", phi2)
