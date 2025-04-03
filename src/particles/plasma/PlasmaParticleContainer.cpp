309e1edaa83ee97a51da19187483ffea48296050
EyaDammak <eya.dammak@ikmail.com>
2025-04-03 17:14:37 +0200

    ppc updated

--- src/particles/plasma/PlasmaParticleContainer.cpp
+++ src/particles/plasma/PlasmaParticleContainer.cpp
@@ -745,8 +745,6 @@ InjectionCondition (const int islice)
     if (!m_can_laser_injection || !laser.UseLaser(islice)) return;
     HIPACE_PROFILE("PlasmaParticleContainer::InjectionCondition()");
 
-    const amrex::Real clight_inv = 1.0_rt/phys_const.c;
-
     for (PlasmaParticleIterator pti(*this); pti.isValid(); ++pti)
     {
         const auto ptd_plasma = pti.GetParticleTile().getParticleTileData();
@@ -756,6 +754,9 @@ InjectionCondition (const int islice)
         amrex::ReduceOps<amrex::ReduceOpSum> reduce_op;
         amrex::ReduceData<uint64_t> reduce_data(reduce_op);
         using ReduceTuple = typename decltype(reduce_data)::Type;
+
+        const PhysConst phys_const = get_phys_const();
+        const amrex::Real clight_inv = 1.0_rt/phys_const.c;
 
         // This kernel calculates the number of ionized electrons in the plasma container
         reduce_op.eval(
@@ -770,7 +771,7 @@ InjectionCondition (const int islice)
                 amrex::Real condition = psi - gamma + uz;
 
                 if (condition < 0){
-                    amrex::ParticleIDWrapper{idcpu_elec[pidx]} = 3;
+                    ptd_plasma.id(ip) = 3;
                 }
         });
     }
@@ -786,7 +787,6 @@ PlasmaToBeam (const MultiLaser& laser, amrex::Vector<amrex::Geometry> const& gm,
     HIPACE_PROFILE("PlasmaParticleContainer::PlasmaToBeam()");
 
     uint32_t num_new_beam_part = 0;
-    const amrex::Real clight_inv = 1.0_rt/phys_const.c;
 
     // Loop over plasma particle boxes
     for (PlasmaParticleIterator pti(*this); pti.isValid(); ++pti)
@@ -843,6 +843,7 @@ PlasmaToBeam (const MultiLaser& laser, amrex::Vector<amrex::Geometry> const& gm,
         const PhysConst phys_const = get_phys_const();
         amrex::Gpu::DeviceScalar<uint32_t> ip_beam(0);
         uint32_t * AMREX_RESTRICT p_ip_beam = ip_beam.dataPtr();
+        const amrex::Real clight_inv = 1.0_rt/phys_const.c;
 
         // This kernel does the transfer of the ionized electrons from the plasma container
         // to the beam container and make them invalid in the plasma container
