a7333c7295d316288508086f7f8421948df12c49
EyaDammak <eya.dammak@ikmail.com>
2025-04-09 09:28:50 +0200

    style

--- src/particles/plasma/PlasmaParticleContainer.cpp
+++ src/particles/plasma/PlasmaParticleContainer.cpp
@@ -763,7 +763,7 @@ InjectionCondition (const MultiLaser& laser, const int islice)
                 amrex::Real psi = ptd_plasma.rdata(PlasmaIdx::psi)[ip];
                 amrex::Real gamma_psi = 0.5_rt ( (1+ ux*ux + uy*uy)/(psi*psi) + 1) // gamma/(1+psi)
                 amrex::Real condition = gamma_psi - m_max_qsa_weighting_factor; // condition for injection
-                
+
                 if (condition > 0 && ptd_plasma.id(ip).is_valid()){
                     ptd_plasma.id(ip) = 3; // set the injected electron ID to 3
                 }
