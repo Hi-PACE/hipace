3fd8f3a174f9d412fbfa767050b93ed7f226b5f9
AlexanderSinn <alexander.sinn@desy.de>
2025-04-16 17:35:50 +0200

    test HIP fix 2

--- .github/workflows/setup/hip.sh
+++ .github/workflows/setup/hip.sh
@@ -36,7 +36,9 @@ sudo apt-key add rocm.gpg.key
 
 source /etc/os-release  # set UBUNTU_CODENAME: focal or jammy or ...
 
-echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/${1-latest} ${UBUNTU_CODENAME} main" \
+VERSION=${1-6.3.2}
+
+echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/${VERSION} ${UBUNTU_CODENAME} main" \
   | sudo tee /etc/apt/sources.list.d/rocm.list
 echo 'export PATH=/opt/rocm/llvm/bin:/opt/rocm/bin:/opt/rocm/profiler/bin:/opt/rocm/opencl/bin:$PATH' \
   | sudo tee -a /etc/profile.d/rocm.sh
@@ -58,10 +60,10 @@ sudo apt-get install -y --no-install-recommends \
     libzstd-dev     \
     ninja-build     \
     openmpi-bin     \
-    rocm-dev        \
-    rocfft-dev      \
-    rocprim-dev     \
-    rocrand-dev
+    rocm-dev${VERSION}    \
+    rocfft-dev${VERSION}  \
+    rocprim-dev${VERSION} \
+    rocrand-dev${VERSION}
 
 # activate
 #
