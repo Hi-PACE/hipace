edbcd3be25d7c1ed2ee4ee9517247e8bddebc974
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2025-04-30 17:47:32 +0200

    Update Laser.cpp

--- src/laser/Laser.cpp
+++ src/laser/Laser.cpp
@@ -36,8 +36,8 @@ Laser::Laser (std::string name,  amrex::Geometry laser_geom_3D)
         queryWithParser(pp, "CEP", m_CEP);
         queryWithParser(pp, "propagation_angle_yz", m_propagation_angle_yz);
         queryWithParser(pp, "STC_theta_xy", m_STC_theta_xy);
-        bool length_is_specified = queryWithParser(pp, "L0", m_L0);
-        bool duration_is_specified = queryWithParser(pp, "tau", m_tau);
+        int length_is_specified = queryWithParser(pp, "L0", m_L0);
+        int duration_is_specified = queryWithParser(pp, "tau", m_tau);
         AMREX_ALWAYS_ASSERT_WITH_MESSAGE( length_is_specified + duration_is_specified == 1,
         "Please specify exclusively either the pulse length L0 or the duration tau of Gaussian lasers");
         if (duration_is_specified) m_L0 = m_tau * get_phys_const().c;
