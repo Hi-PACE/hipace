43796f3c1df67e2db01f0f9747243032da46fe6c
EyaDammak <eya.dammak@ikmail.com>
2025-05-09 14:38:45 +0200

    fix issue

--- src/particles/plasma/MultiPlasma.cpp
+++ src/particles/plasma/MultiPlasma.cpp
@@ -153,7 +153,7 @@ MultiPlasma::DoLaserIonization (
 
 void
 MultiPlasma::DoLaserInjection (
-    const int lev, const MultiLaser& laser, const Fields& fields, amrex::Vector<amrex::Geometry> const& gm, const int islice)
+    const int lev, const Fields& fields, const MultiLaser& laser, amrex::Vector<amrex::Geometry> const& gm, const int islice)
 {
     for (auto& plasma : m_all_plasmas) {
         plasma.InjectionCondition(lev, fields, laser, islice);
