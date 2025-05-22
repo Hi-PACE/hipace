b4cec7004905509a9b94a6fb0b5b7f7d916b20d5
Eya D. <81635404+EyaDammak@users.noreply.github.com>
2025-05-22 13:11:15 +0200

    Update analysis_laser_ionization.py

--- examples/laser_ionization/analysis_laser_ionization.py
+++ examples/laser_ionization/analysis_laser_ionization.py
@@ -9,7 +9,6 @@ import argparse
 import numpy as np
 import math
 from openpmd_viewer import OpenPMDTimeSeries
-import statistics
 import scipy.constants as scc
 
 import sys
@@ -48,15 +47,16 @@ nc = 1.75e27
 n0 = nc / 10000
 
 iteration = 0
+izmax = 10
 
 rho_elec_linear, _ = ts_linear.get_field(field='rho_elec', coord='z', iteration=iteration)
 rho_elec_mean_linear = np.mean(rho_elec_linear, axis=(1, 2))
-rho_average_linear = statistics.mean(rho_elec_mean_linear[0:10])
+rho_average_linear = np.mean(rho_elec_mean_linear[0:izmax])
 fraction_linear = -rho_average_linear / scc.e / n0
 
 rho_elec_circular, _ = ts_circular.get_field(field='rho_elec', coord='z', iteration=iteration)
 rho_elec_mean_circular = np.mean(rho_elec_circular, axis=(1, 2))
-rho_average_circular = statistics.mean(rho_elec_mean_circular[0:10]) #average over a thickness in the ionized region
+rho_average_circular = np.mean(rho_elec_mean_circular[0:izmax]) #average over a thickness in the ionized region
 fraction_circular = -rho_average_circular / scc.e / n0
 
 fraction_warpx_linear = 0.41014984 # result from WarpX simulation
@@ -74,59 +74,47 @@ print(f"fraction_hipace_circular = {fraction_circular}")
 # in-situ diagnostics for calculation of the temperature in all directions in circular polarization
 insitu_path_linear = f'./{args.third}/reduced_elec.0000.txt'
 all_data_linear = diag.read_file(insitu_path_linear)
-Tx2_l = all_data_linear['[ux^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-Ty2_l = all_data_linear['[uy^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-Tz2_l = all_data_linear['[uz^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-temp_eV_linear = 1./3*(Tx2_l+Ty2_l+Tz2_l)
+Tx2 = all_data_linear['[ux^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+Ty2 = all_data_linear['[uy^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+Tz2 = all_data_linear['[uz^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+temp_eV_linear = 1./3*(Tx2+Ty2+Tz2)
 
 insitu_path_circular = f'./{args.fourth}/reduced_elec.0000.txt'
 all_data_circular = diag.read_file(insitu_path_circular)
-Tx2_c = all_data_circular['[ux^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-Ty2_c = all_data_circular['[uy^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-Tz2_c = all_data_circular['[uz^2]'][0,0]*scc.m_e*scc.c**2/scc.e
-temp_eV_circular = 1./3*(Tx2_c+Ty2_c+Tz2_c)
+Tx2 = all_data_circular['[ux^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+Ty2 = all_data_circular['[uy^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+Tz2 = all_data_circular['[uz^2]'][0,0]*scc.m_e*scc.c**2/scc.e
+temp_eV_circular = 1./3*(Tx2+Ty2+Tz2)
 
 # calculation of temperature with OpenPMD-viewer diagnostics
-wf_l, _ = ts_linear.get_field(field='w_elec', iteration=iteration)
-
+wf, _ = ts_linear.get_field(field='w_elec', iteration=iteration)
+sum_wf = np.sum(wf, axis=(1,2))[0:izmax]
 ux2_linear, _ = ts_linear.get_field(field='ux^2_elec', iteration=iteration)
-ux2_mean_linear = np.sum(np.sum(ux2_linear*wf_l, axis=1),axis=1) / np.sum(np.sum(wf_l, axis=1),axis=1)
-ux2_average_linear = statistics.mean(ux2_mean_linear[0:10])
-
 uy2_linear, _ = ts_linear.get_field(field='uy^2_elec', iteration=iteration)
-uy2_mean_linear = np.sum(np.sum(uy2_linear*wf_l, axis=1),axis=1) / np.sum(np.sum(wf_l, axis=1),axis=1)
-uy2_average_linear = statistics.mean(uy2_mean_linear[0:10])
-
 uz2_linear, _ = ts_linear.get_field(field='uz^2_elec', iteration=iteration)
-uz2_mean_linear = np.sum(np.sum(uz2_linear*wf_l, axis=1),axis=1) / np.sum(np.sum(wf_l, axis=1),axis=1)
-uz2_average_linear = statistics.mean(uz2_mean_linear[0:10])
+ux2_average_linear = np.mean(np.sum(ux2_linear * wf, axis=(1, 2))[0:izmax] / sum_wf)
+uy2_average_linear = np.mean(np.sum(uy2_linear * wf, axis=(1, 2))[0:izmax] / sum_wf)
+uz2_average_linear = np.mean(np.sum(uz2_linear * wf, axis=(1, 2))[0:izmax] / sum_wf)
+temp_diags_linear = 1./3*(ux2_average_linear + uy2_average_linear + uz2_average_linear)*scc.m_e*scc.c**2/scc.e
 
-temp_diags_linear = 1./3*(ux2_average_linear + uy2_average_linear +uz2_average_linear)*scc.m_e*scc.c**2/scc.e
-
-wf_c, _ = ts_circular.get_field(field='w_elec', iteration=iteration)
-
+wf, _ = ts_circular.get_field(field='w_elec', iteration=iteration)
+sum_wf = np.sum(wf, axis=(1,2))[0:izmax]
 ux2_circular, _ = ts_circular.get_field(field='ux^2_elec', iteration=iteration)
-ux2_mean_circular = np.sum(np.sum(ux2_circular*wf_c, axis=1),axis=1) / np.sum(np.sum(wf_c, axis=1),axis=1)
-ux2_average_circular = statistics.mean(ux2_mean_circular[0:10])
-
 uy2_circular, _ = ts_circular.get_field(field='uy^2_elec', iteration=iteration)
-uy2_mean_circular = np.sum(np.sum(uy2_circular*wf_c, axis=1),axis=1) / np.sum(np.sum(wf_c, axis=1),axis=1)
-uy2_average_circular = statistics.mean(uy2_mean_circular[0:10])
-
 uz2_circular, _ = ts_circular.get_field(field='uz^2_elec', iteration=iteration)
-uz2_mean_circular = np.sum(np.sum(uz2_circular*wf_c, axis=1),axis=1) / np.sum(np.sum(wf_c, axis=1),axis=1)
-uz2_average_circular = statistics.mean(uz2_mean_circular[0:10])
-
-temp_diags_circular = 1./3*(ux2_average_circular + uy2_average_circular +uz2_average_circular)*scc.m_e*scc.c**2/scc.e
+ux2_average_circular = np.mean(np.sum(ux2_circular * wf, axis=(1, 2))[0:izmax] / sum_wf)
+uy2_average_circular = np.mean(np.sum(uy2_circular * wf, axis=(1, 2))[0:izmax] / sum_wf)
+uz2_average_circular = np.mean(np.sum(uz2_circular * wf, axis=(1, 2))[0:izmax] / sum_wf)
+temp_diags_circular = 1./3*(ux2_average_circular + uy2_average_circular + uz2_average_circular)*scc.m_e*scc.c**2/scc.e
 
 temp_eV_warpx_linear = 1.00286009
 temp_eV_warpx_circular = 9.68224535
 
-print(f"temperature (eV) Warpx, linear = {temp_eV_warpx_linear}")
+print(f"temperature (eV) WarpX, linear = {temp_eV_warpx_linear}")
 print(f"temperature (eV) HiPACE++, linear with insitu diagnostics = {temp_eV_linear}")
 print(f"temperature (eV) HiPACE++, linear with Open-PMD diagnostics = {temp_diags_linear}")
 
-print(f"temperature (eV) Warpx, circular = {temp_eV_warpx_circular}")
+print(f"temperature (eV) WarpX, circular = {temp_eV_warpx_circular}")
 print(f"temperature (eV) HiPACE++, circular with insitu diagnostics = {temp_eV_circular}")
 print(f"temperature (eV) HiPACE++, circular with Open-PMD diagnostics = {temp_diags_circular}")
 
