c3c86ffc42fdbd66b206293a2833a4108e8e8146
huixingjian <xingjian.hui@desy.de>
2025-12-08 22:05:21 +0100

    add flag to diable laser evolution

--- src/Hipace.cpp
+++ src/Hipace.cpp
@@ -243,7 +243,7 @@ Hipace::ReadParameters ()
     m_multi_laser.MakeLaserGeometry(m_3D_geom[0]);
 
     m_use_laser = m_multi_laser.UseLaser();
-
+    queryWithParser(pph, "disable_laser_solver", m_disable_laser_solver);
     queryWithParser(pph, "collisions", m_collision_names);
     /** Initialize the collision objects */
     m_ncollisions = m_collision_names.size();
@@ -731,8 +731,9 @@ Hipace::SolveOneSlice (int islice, int step)
 
     // Advance laser slice by 1 step using chi
     // no MR for laser
+    if (!m_disable_laser_solver){
     m_multi_laser.AdvanceSlice(islice, m_fields, m_dt, step, m_3D_geom[0]);
-
+    }
     if (islice-1 >= m_3D_geom[0].Domain().smallEnd(2)) {
         m_multi_buffer.get_data(islice-1, m_multi_beam, m_multi_laser, WhichBeamSlice::Next);
         m_multi_beam.ReorderParticles( WhichBeamSlice::Next, step, m_slice_geom[0]);
