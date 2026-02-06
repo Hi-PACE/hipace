b528bf454115f437cda1d34931e352641157d84a
Xingjian Hui <xingjianhui@mpamb66051-w.desy.de>
2026-02-06 13:49:59 +0100

    add one more element for DepositCurrent

--- src/fields/Fields.cpp
+++ src/fields/Fields.cpp
@@ -103,7 +103,7 @@ Fields::AllocData (
             if (Hipace::m_deposit_rho_ion_levels) {
                 for (auto& pc : Hipace::GetInstance().m_multi_plasma.m_all_plasmas ) {
                     const std::string& plasma_name = pc.GetName();
-                    if(pc.GetCharge()==-1){continue;}
+                    if(pc.m_max_ion_lev == 0){continue;}
                     for (int ion_lev=1; ion_lev<=pc.m_max_ion_lev; ion_lev++){
                         Comps[isl].multi_emplace(N_Comps, "rho_" + plasma_name + "_lev"+ std::to_string(ion_lev));
                     }
@@ -167,6 +167,15 @@ Fields::AllocData (
             if (Hipace::m_deposit_rho_individual) {
                 for (auto& plasma_name : Hipace::GetInstance().m_multi_plasma.GetNames()) {
                     Comps[isl].multi_emplace(N_Comps, "rho_" + plasma_name);
+                }
+            }
+            if (Hipace::m_deposit_rho_ion_levels) {
+                for (auto& pc : Hipace::GetInstance().m_multi_plasma.m_all_plasmas ) {
+                    const std::string& plasma_name = pc.GetName();
+                    if(pc.m_max_ion_lev == 0){continue;}
+                    for (int ion_lev=1; ion_lev<=pc.m_max_ion_lev; ion_lev++){
+                        Comps[isl].multi_emplace(N_Comps, "rho_" + plasma_name + "_lev"+ std::to_string(ion_lev));
+                    }
                 }
             }
             if (Hipace::m_deposit_temp_individual) {
