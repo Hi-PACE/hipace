41ca6ba53062ac30e70aacc104fefd15df7d5ddb
Xingjian Hui <xingjianhui@mpamb66051-w.desy.de>
2026-02-06 14:38:45 +0100

    NVCC lambda issue

--- src/particles/deposition/PlasmaDepositCurrent.cpp
+++ src/particles/deposition/PlasmaDepositCurrent.cpp
@@ -180,9 +180,11 @@ DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
                 amrex::Real q_invvol = charge_invvol * ptd.rdata(PlasmaIdx::w)[ip];
                 amrex::Real q_mu0_mass_ratio = charge_mu0_mass_ratio;
                 [[maybe_unused]] amrex::Real laser_norm_ion = laser_norm;
+                const bool dep_rho_ion_levels = deposit_rho_ion_levels;
+                const int ion_lev_local = ion_lev;
                 if constexpr (can_ionize) {
                     const int p_ion_lev= ptd.idata(PlasmaIdx::ion_lev)[ip];
-                    if ( deposit_rho_ion_levels && ion_lev != p_ion_lev) return;
+                    if ( dep_rho_ion_levels && ion_lev_local != p_ion_lev) return;
                     q_invvol *= p_ion_lev;
                     q_mu0_mass_ratio *=  p_ion_lev;
                     laser_norm_ion *= p_ion_lev *  p_ion_lev;
@@ -192,6 +194,7 @@ DepositCurrent (PlasmaParticleContainer& plasma, Fields & fields,
                 const amrex::Real ymid = (yp - y_pos_offset) * dy_inv;
 
                 amrex::Real Aabssqp = 0._rt;
+
                 if constexpr (use_laser) {
                     doLaserGatherShapeN<depos_order>(xp, yp, Aabssqp, arr, cache_idx[0],
                                                     dx_inv, dy_inv, x_pos_offset, y_pos_offset);
