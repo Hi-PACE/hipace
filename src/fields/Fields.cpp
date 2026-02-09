27b7b1cc7a582b95233917b3f01c3bf21d4349f5
Xingjian Hui <xingjianhui@mpamb66051-w.desy.de>
2026-02-09 16:26:48 +0100

    add debug lines

--- src/fields/Fields.cpp
+++ src/fields/Fields.cpp
@@ -173,12 +173,16 @@ Fields::AllocData (
                     Comps[isl].multi_emplace(N_Comps, "rho_" + plasma_name);
                 }
             }
+            amrex::Print()<<"deposit_rho_ion_levels is "<<Hipace::m_deposit_rho_ion_levels;
             if (Hipace::m_deposit_rho_ion_levels) {
                 for (auto& pc : Hipace::GetInstance().m_multi_plasma.m_all_plasmas ) {
                     const std::string& plasma_name = pc.GetName();
                     if(pc.m_max_ion_lev == 0){continue;}
                     for (int ion_lev=1; ion_lev<=pc.m_max_ion_lev; ion_lev++){
                         Comps[isl].multi_emplace(N_Comps, "rho_" + plasma_name + "_IL"+ std::to_string(ion_lev));
+                        amrex::Print() << "Inserted? "
+                                        << Comps[isl].count("rho_" + plasma_name + "_IL"+ std::to_string(ion_lev))
+                                        << "  key=" << "rho_" + plasma_name + "_IL"+ std::to_string(ion_lev) << "\n";
                     }
                 }
             }
