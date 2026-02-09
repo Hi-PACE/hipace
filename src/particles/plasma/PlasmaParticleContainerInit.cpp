091b6898916d0a2082ca63072824ea42997a14f6
Maxence Thévenet <maxence.thevenet@desy.de>
2026-02-09 19:37:24 +0100

    should not have deleted

--- src/particles/plasma/PlasmaParticleContainerInit.cpp
+++ src/particles/plasma/PlasmaParticleContainerInit.cpp
@@ -468,6 +468,7 @@ InitIonizationModule (const amrex::Geometry& geom, const amrex::Real background_
             * std::pow(2*std::pow((Uion/UH),3./2.)*Ea,2*n_eff - 1);
         m_adk_exp_prefactor[i] = -2./3. * std::pow( Uion/UH,3./2.) * Ea;
         m_laser_adk_prefactor[i] = (3./MathConst::pi) * std::pow(Uion/UH, -3./2.) / Ea;
+        m_laser_dp_prefactor[i] = std::sqrt(3./2./Ea) * std::pow(UH/Uion, 3./4.);
     }
 
     m_adk_power.copyToDeviceAsync();
