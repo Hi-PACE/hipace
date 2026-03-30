cbce31c006e4a696913d8625ed9b602b74ab4093
Xingjian Hui <xingjian.hui@desy.de>
2026-03-30 16:27:40 +0200

    add argument to MultiPlasma::DepositCurrent

--- src/particles/plasma/MultiPlasma.cpp
+++ src/particles/plasma/MultiPlasma.cpp
@@ -84,20 +84,20 @@ MultiPlasma::maxChargeDensity (amrex::Real z)
 void
 MultiPlasma::DepositCurrent (
     Fields & fields, int which_slice,
-    bool deposit_jx_jy, bool deposit_jz, bool deposit_rho, bool deposit_chi, bool deposit_rhomjz,
+    bool deposit_jx_jy, bool deposit_jz, bool deposit_rho, bool deposit_chi, bool deposit_rhomjz, bool deposit_n,
     amrex::Vector<amrex::Geometry> const& gm, int const lev)
 {
     for (int i=0; i<m_nplasmas; i++) {
         ::DepositCurrent(m_all_plasmas[i], fields, which_slice,
-                         deposit_jx_jy, deposit_jz, deposit_rho, deposit_chi, deposit_rhomjz,
+                         deposit_jx_jy, deposit_jz, deposit_rho, deposit_chi, deposit_rhomjz, deposit_n,
                          gm, lev, -1);
 
         if (Hipace::m_deposit_n_ion_levels &&
-            deposit_rho &&
+            Hipace::m_deposit_n &&
             m_all_plasmas[i].m_max_ion_lev > 0) {
             for (int ion_lev = 0; ion_lev <= m_all_plasmas[i].m_max_ion_lev; ++ion_lev) {
                 ::DepositCurrent(m_all_plasmas[i], fields, which_slice,
-                    false, false, true, false, false,
+                    false, false, false, false, false, true
                     gm, lev, ion_lev);
             }
         }
