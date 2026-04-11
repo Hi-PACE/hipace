92e74e0efcdbdc7dca6ba0fba036a28d6a97e7d1
Maxence Thévenet <maxence.thevenet@desy.de>
2026-04-11 08:56:44 +0200

    Apply suggestions from code review
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- tests/plasma_ionisation_p_lev.1Rank.sh
+++ tests/plasma_ionisation_p_lev.1Rank.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-# Copyright 2025
+# Copyright 2026
 #
 # This file is part of HiPACE++.
 #
-# Authors: AlexanderSinn, MaxThevenet
+# Authors: Xingjian Hui, AlexanderSinn, MaxThevenet
 # License: BSD-3-Clause-LBNL
 
 # abort on first encounted error
@@ -18,16 +18,16 @@ HIPACE_EXAMPLE_DIR=${HIPACE_SOURCE_DIR}/examples/plasma_ionization
 GRID_DIAGS="${PWD}/grid_ionization.1Rank"
 PARTICLE_DIAGS="${PWD}/particle_ionization.1Rank"
 
-mpiexec -n 1 "$HIPACE_EXECUTABLE" "$HIPACE_EXAMPLE_DIR/inputs_plasma_ionization" \
-    hipace.file_prefix="$GRID_DIAGS" \
-    ion.ppc="0 0" \
+mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_plasma_ionization \
+    hipace.file_prefix=$GRID_DIAGS \
+    ion.ppc=0 0 \
     grid_ionization.plasma_names=ion
 
-mpiexec -n 1 "$HIPACE_EXECUTABLE" "$HIPACE_EXAMPLE_DIR/inputs_plasma_ionization" \
-    hipace.file_prefix="$PARTICLE_DIAGS" \
+mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_plasma_ionization \
+    hipace.file_prefix=$PARTICLE_DIAGS \
     hipace.deposit_n=1 \
     hipace.deposit_n_ion_levels=1
 
-python3 "$HIPACE_EXAMPLE_DIR/analysis_plasma_ionization.py" \
-    --diags_grid="$GRID_DIAGS" \
-    --diags_particle="$PARTICLE_DIAGS"
+python3 $HIPACE_EXAMPLE_DIR/analysis_plasma_ionization.py \
+    --diags_grid=$GRID_DIAGS \
+    --diags_particle=$PARTICLE_DIAGS
