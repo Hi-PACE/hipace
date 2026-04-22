1bb71137b7f56c02e0e746c98ad29139b11978f9
Alexander Sinn <alexander.sinn@desy.de>
2026-04-22 15:48:13 +0200

    time laser STC test

--- tests/laser_STC.1Rank.sh
+++ tests/laser_STC.1Rank.sh
@@ -22,7 +22,11 @@ TEST_NAME="${FILE_NAME%.*}"
 rm -rf $TEST_NAME
 
 # Run the simulation
+time \
 mpiexec -n 1 $HIPACE_EXECUTABLE $HIPACE_EXAMPLE_DIR/inputs_STC \
         hipace.file_prefix = $TEST_NAME
 # Compare the result with theory
+time \
 $HIPACE_EXAMPLE_DIR/analysis_laser_STC.py --output-dir=$TEST_NAME
+
+exit 1;
